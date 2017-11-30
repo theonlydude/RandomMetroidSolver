2b778915479c8336ef74cef1caa8be281c847c47
theonlydude <you@example.com>
2017-11-30 21:02:59 +0100

    run_tests.sh must be launched from the main directory, not the tests one.

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+
+# !! must be launched from RandomMetroidSolver directory, not tests directory !!
+
 python -m unittest discover
 
-for ROM in roms/*; do
+for ROM in tests/roms/*; do
     echo "${ROM}"
-    python2 ../solver.py "${ROM}"
-    python3 ../solver.py "${ROM}"
+    python2 solver.py "${ROM}"
+    python3 solver.py "${ROM}"
 done

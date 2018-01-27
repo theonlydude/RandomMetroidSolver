f9ef767fc60163cb4ebfaf0e96173832268bd264
theonlydude <you@example.com>
2018-01-27 21:41:32 +0100

    solver: add util functions to SMBool

--- smbool.py
+++ smbool.py
@@ -4,3 +4,14 @@ class SMBool:
         self.bool = bool
         self.difficulty = difficulty
         self.knows = knows
+
+    def __repr__(self):
+        # to display the smbool as a string
+        return '({}, {})'.format(self.bool, self.difficulty)
+
+    def __getitem__(self, index):
+        # to acces the smbool as [0] for the bool and [1] for the difficulty
+        if index == 0:
+            return self.bool
+        elif index == 1:
+            return self.difficulty

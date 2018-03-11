2bcf2ed22b553de1bfe288cccf3b09d89abffb10
theonlydude <you@example.com>
2018-03-11 21:29:11 +0100

    stats: add multi cpu and pypy support.

--- collect_stats.bash
+++ collect_stats.bash
@@ -11,6 +11,8 @@ n=20
 
 test_set=$1
 
+nb_cpu=$(grep processor /proc/cpuinfo | wc -l)
+
 [ -z "$test_set" ] && test_set="seeds"
 
 [ -d "$test_set" ] && {
@@ -22,20 +24,39 @@ mkdir -p $test_set/old_seeds
 mv Ouiche_Randomizer_*.sfc $test_set/old_seeds 2> /dev/null
 rmdir $test_set/old_seeds 2> /dev/null
 
+which pypy && PYPY="pypy"
+
+function worker {
+    p=$1
+    speed=$2
+    i=$3
+    dest=$4
+
+    echo
+    echo "**** $p $speed $i ****"
+    echo
+    seed=$(head -c 500 /dev/urandom | tr -dc '0-9' | fold -w 7 | head -n 1)
+    ${PYPY} $RANDO --seed ${seed} -i $speed --param diff_presets/$p.json -c AimAnyButton.ips -c itemsounds.ips -c max_ammo_display.ips -c spinjumprestart.ips --rom ~/roms/Super\ Metroid\ \(Japan\,\ USA\)\ \(En\,Ja\).sfc
+    solver_log="$dest/${seed}.txt"
+    rom="Ouiche_Randomizer_X${seed}_${p}.sfc"
+    $SOLVER $rom --param diff_presets/$p.json  --difficultyTarget 5 --displayGeneratedPath > $solver_log
+    mv $rom $dest
+}
+
 for p in $presets; do
     mkdir $test_set/$p
     for speed in $progs; do
 	dest=$test_set/$p/$speed
 	mkdir $dest
+	workers=0
 	for i in $(seq 1 $n); do
-	    echo
-	    echo "**** $p $speed $i ****"
-	    echo
-	    $RANDO -i $speed --param diff_presets/$p.json -c AimAnyButton.ips -c itemsounds.ips -c max_ammo_display.ips -c spinjumprestart.ips  --rom ~/roms/Super\ Metroid\ \(Japan\,\ USA\)\ \(En\,Ja\).sfc
-	    seed=Ouiche_Randomizer_*.sfc
-	    solver_log="$dest/$(basename -s .sfc $seed).txt"
-	    $SOLVER $seed --param diff_presets/$p.json  --difficultyTarget 5 --displayGeneratedPath > $solver_log
-	    mv $seed $dest
+	    if [ ${workers} -lt ${nb_cpu} ]; then
+		worker ${p} ${speed} ${i} ${dest} &
+		let workers=${workers}+1
+	    else
+		wait
+		workers=0
+	    fi
 	done
 	$GET_STATS $dest/*.sfc
 	mv *stats.csv $dest

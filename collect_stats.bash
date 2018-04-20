d59fb6e3368a6b9a473ea98784d913dc25888a58
flo <flo@bite.ta>
2018-04-20 20:23:34 +0200

    collect_stats; generated json ROMs, use 1st item log for stats

--- collect_stats.bash
+++ collect_stats.bash
@@ -23,11 +23,8 @@ nb_cpu=$(grep processor /proc/cpuinfo | wc -l)
 }
 
 mkdir -p $test_set/old_seeds
-mv VARIA_Randomizer_*.sfc $test_set/old_seeds 2> /dev/null
+mv VARIA_Randomizer_*.json $test_set/old_seeds 2> /dev/null
 rmdir $test_set/old_seeds 2> /dev/null
-
-which pypy && PYPY="pypy"
-
 abort_msg=
 
 function worker {
@@ -41,11 +38,12 @@ function worker {
     echo "**** $dest $i ****"
     echo $extra
     seed=$(head -c 500 /dev/urandom | tr -dc '0-9' | fold -w 7 | head -n 1 | sed 's/^0*//')
-    ${PYPY} $RANDO --seed ${seed} -i $speed $extra --param diff_presets/$p.json -c AimAnyButton.ips -c itemsounds.ips -c max_ammo_display.ips -c skip_ceres.ips -c elevators_doors_speed.ips --rom ~/roms/Super\ Metroid\ \(Japan\,\ USA\)\ \(En\,Ja\).sfc
-    solver_log="$dest/${seed}.txt"
-    rom="VARIA_Randomizer_*${seed}_${p}*.sfc"
+    $RANDO --seed ${seed} -i $speed $extra --param diff_presets/$p.json
+    rom=$(ls VARIA_Randomizer_*${seed}_${p}*.json)
+    rom1st=${dest}/${rom/json/1st}
+    solver_log=${dest}/${rom/json/log}
     ls $rom > /dev/null 2>&1 && {
-	$SOLVER $rom --param diff_presets/$p.json  --difficultyTarget 5 --displayGeneratedPath > $solver_log
+	$SOLVER $rom --param diff_presets/$p.json  --difficultyTarget 5 --displayGeneratedPath -1 $rom1st > $solver_log
 	[ $? -ne 0 ] && {
 	    abort_msg="Could not solve $rom !! $abort_msg"
 	    return
@@ -60,7 +58,7 @@ function gen_seeds() {
     for p in $presets; do
 	mkdir -p $base_dir/$p
 	for speed in $progs; do
-	    #	    extra="$base_extra --graph"
+#	    extra="$base_extra --graph"
 	    extra="$base_extra"
 	    if [[ $speed != "random" ]]; then
 		extra="$extra --speedScrewRestriction"
@@ -93,7 +91,7 @@ function gen_seeds() {
 		echo $abort_msg
 		exit 1
 	    }
-	    $GET_STATS $dest/*.sfc
+	    $GET_STATS $dest/*.1st
 	    mv *stats.csv $dest
 	done
     done

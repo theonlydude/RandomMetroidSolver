750c0b0eaef5bd7d4bc5b80e2bc2f52c642a9964
flo <flo@bite.ta>
2018-06-04 00:41:36 +0200

    boss_test! updated due to jm

--- boss_test.py
+++ boss_test.py
@@ -4,6 +4,7 @@ import sys
 
 from parameters import *
 from helpers import *
+from smboolmanager import *
 from solver import ParamsLoader
 
 def stuff(base, nEtanks, nMissiles, nSupers, nPowerBombs):
@@ -25,6 +26,7 @@ itemSets = {
         'ChargeLess' : stuff(['Varia'], 5, 6, 3, 1),
         'Tough' : stuff(['Charge', 'Wave'], 5, 3, 1, 1),
         'VeryTough': stuff(['Charge'], 1, 2, 1, 1),
+        'NoTanks': stuff(['Charge'], 0, 2, 1, 1),
     },
     'Draygon' : {
         'Standard' : stuff(['Spazer', 'Charge', 'Wave', 'Ice', 'Varia', 'Gravity'], 7, 10, 4, 3),
@@ -34,8 +36,7 @@ itemSets = {
     'Ridley' : {
         'Standard' : stuff(['Plasma', 'Charge', 'Wave', 'Ice', 'Varia', 'Gravity'], 7, 10, 6, 3),
         'ChargeLess' : stuff(['Varia', 'Gravity'], 9, 10, 9, 3),
-        'Tough' : stuff(['Spazer', 'Charge', 'Wave', 'Ice', 'Varia', 'Gravity'], 4, 10, 3, 3),
-        'TX4633580' : stuff(['Spazer', 'Charge', 'Plasma', 'Ice', 'Varia', 'Gravity'], 13, 10, 10, 4)
+        'Tough' : stuff(['Spazer', 'Charge', 'Wave', 'Ice', 'Varia', 'Gravity'], 4, 10, 3, 3)
     },
     'MotherBrain' : {
         'Standard' : stuff(['Plasma', 'Charge', 'Wave', 'Ice', 'Varia', 'Gravity'], 9, 10, 6, 3),
@@ -43,6 +44,8 @@ itemSets = {
         'Tough' : stuff(['Spazer', 'Charge', 'Wave', 'Ice', 'Varia', 'Gravity'], 5, 10, 5, 3)
     }
 }
+
+sm = SMBoolManager.factory('diff')
 
 def boss(name, diffFunction):
     with open(name + ".csv", "w") as csvOut:
@@ -55,7 +58,9 @@ def boss(name, diffFunction):
             for setName, itemSet in itemSets[name].items():
                 print('* Item set ' + setName)
                 #        print(str(itemSet))
-                d = diffFunction(itemSet)
+                sm.resetItems()
+                sm.addItems(itemSet)
+                d = diffFunction()
                 print('---> ' + str(d))
                 csvOut.write(presetName + ";" + setName + ";" + str(d[0]) + ";" + str(d[1]) + "\n")
 
@@ -63,9 +68,10 @@ if __name__ == "__main__":
     if len(sys.argv) >= 2:
         params = sys.argv[1]
         ParamsLoader.factory(params).load()
-    boss('Kraid', enoughStuffsKraid)
-    boss('Phantoon', enoughStuffsPhantoon)
-    boss('Draygon', enoughStuffsDraygon)
-    boss('Ridley', enoughStuffsRidley)
-    boss('MotherBrain', enoughStuffsMotherbrain)
+    h = Helpers(sm)
+    boss('Kraid', h.enoughStuffsKraid)
+    boss('Phantoon', h.enoughStuffsPhantoon)
+    boss('Draygon', h.enoughStuffsDraygon)
+    boss('Ridley', h.enoughStuffsRidley)
+    boss('MotherBrain', h.enoughStuffsMotherbrain)
 

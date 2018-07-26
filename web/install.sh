2bd63d136f6e40de0fc911504d007852575a264b
dude <dude@dude.com>
2018-07-26 11:03:40 +0200

    web: .sh to create symlinks in web2py

---
+++ web/install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+ln -s ~/RandomMetroidSolver/web/static/favicon.ico ~/web2py/applications/solver/static/favicon.ico
+ln -s ~/RandomMetroidSolver/web/static/area_map_thumbnail.png ~/web2py/applications/solver/static/images/area_map_thumbnail.png
+ln -s ~/RandomMetroidSolver/web/static/area_map.png ~/web2py/applications/solver/static/images/area_map.png
+ln -s ~/RandomMetroidSolver/web/static/snes-controller.png ~/web2py/applications/solver/static/images/snes-controller.png
+ln -s ~/RandomMetroidSolver/web/static/ajax-loader.gif ~/web2py/applications/solver/static/images/ajax-loader.gif
+ln -s ~/RandomMetroidSolver/web/static/mystyle.css ~/web2py/applications/solver/static/css/mystyle.css
+ln -s ~/RandomMetroidSolver/web/static/bootstrap-tour.min.css ~/web2py/applications/solver/static/css/bootstrap-tour.min.css
+ln -s ~/RandomMetroidSolver/web/static/barrating ~/web2py/applications/solver/static/barrating
+ln -s ~/RandomMetroidSolver/web/static/bootstrap-tour.min.js ~/web2py/applications/solver/static/js/bootstrap-tour.min.js
+ln -s ~/RandomMetroidSolver/web/static/FileSaver.js ~/web2py/applications/solver/static/js/FileSaver.js
+ln -s ~/RandomMetroidSolver/web/static/dist ~/web2py/applications/solver/static/dist
+ln -s ~/RandomMetroidSolver/web/static/highslide ~/web2py/applications/solver/static/highslide
+ln -s ~/RandomMetroidSolver/web/views/home.html ~/web2py/applications/solver/views/solver_web/home.html
+ln -s ~/RandomMetroidSolver/web/views/presets.html ~/web2py/applications/solver/views/solver_web/presets.html
+ln -s ~/RandomMetroidSolver/web/views/solver.html ~/web2py/applications/solver/views/solver_web/solver.html
+ln -s ~/RandomMetroidSolver/web/views/randomizer.html ~/web2py/applications/solver/views/solver_web/randomizer.html
+ln -s ~/RandomMetroidSolver/web/views/infos.html ~/web2py/applications/solver/views/solver_web/infos.html
+ln -s ~/RandomMetroidSolver/web/controllers/solver_web.py ~/web2py/applications/solver/controllers/solver_web.py

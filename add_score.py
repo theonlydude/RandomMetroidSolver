35307a1d53c47383a0a17c7150c4606c92d46486
theonlydude <you@example.com>
2018-08-26 17:31:33 +0200

    web: add home page in the menu.

--- add_score.py
+++ add_score.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python
 
 from utils import PresetLoader
-import os
+import os, sys
 
-def loadPresetsList():
-    return [os.path.join('standard_presets', f) for f in os.listdir('standard_presets') if f != 'solution.json'] + [os.path.join('community_presets', f) for f in os.listdir('community_presets')]
+def loadPresetsList(directory):
+    return [os.path.join(directory, f) for f in os.listdir(directory) if f != 'solution.json']
 
 if __name__ == "__main__":
-    presets = loadPresetsList()
+    if len(sys.argv) != 2:
+        sys.exit()
+
+    presets = loadPresetsList(sys.argv[1])
 
     for preset in presets:
         print(preset)

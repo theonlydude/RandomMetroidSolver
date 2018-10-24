0618447e357eedd364786514ea020db5c29d61c2
flo <flo@bite.ta>
2018-10-25 00:16:40 +0200

    locs: req more tanks for wave area

--- graph_locations.py
+++ graph_locations.py
@@ -380,7 +380,7 @@ locations = [
     'Visibility': "Chozo",
     'Room': 'Wave Beam Room',
     'AccessFrom' : {
-        'Bubble Mountain': lambda sm: sm.canHellRun('MainUpperNorfair')
+        'Bubble Mountain': lambda sm: sm.canHellRun('MainUpperNorfair', 0.75)
     },
     'Available': lambda sm: sm.canOpenRedDoors(),
     'PostAvailable': lambda sm: sm.wor(sm.haveItem('Morph'), # exit through lower passage under the spikes
@@ -1393,7 +1393,7 @@ locations = [
     'Visibility': "Visible",
     'Room': 'Double Chamber',
     'AccessFrom' : {
-        'Bubble Mountain': lambda sm: sm.canHellRun('MainUpperNorfair')
+        'Bubble Mountain': lambda sm: sm.canHellRun('MainUpperNorfair', 0.75)
     },
     'Available': lambda sm: sm.canOpenRedDoors()
 },

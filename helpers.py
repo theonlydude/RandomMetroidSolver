e81f4c3f8e4da1cb31bdc315b67160737593c433
flo <flo@bite.ta>
2019-12-02 22:24:30 +0100

    removed hardcoded diff

--- helpers.py
+++ helpers.py
@@ -87,7 +87,7 @@ class Helpers(object):
 
         isHeatProof = sm.heatProof()
         if isHeatProof == True:
-            return SMBool(isHeatProof.bool, easy, isHeatProof.knows, isHeatProof.items)
+            return SMBool(isHeatProof.bool, 0, isHeatProof.knows, isHeatProof.items)
         elif self.energyReserveCount() >= minE:
             if hellRun != 'LowerNorfair':
                 return self.energyReserveCountOkHellRun(hellRun, mult)

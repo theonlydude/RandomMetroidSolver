03d768b76e49f83434a8991adb8d816ff9dd2168
flo <flo@bite.ta>
2019-12-11 22:23:03 +0100

    patches: changed nerfed charge, SBAs take 3 PBs instead of doing 1/3 damage.

--- itemrandomizerweb/patches/nerfed_charge.asm
+++ itemrandomizerweb/patches/nerfed_charge.asm
@@ -14,19 +14,6 @@
 lorom
 arch snes.cpu
 
-;;; goes to charge branch whatever items
-org $90b81e
-	bit #$0000
-	bra $0a
-
-;;; disables a "no charge" check
-org $90b8f2
-	bra $00
-
-;;; hijack for beam damage modification 
-org $90b9e6
-	jsr charge
-
 ;;; divides projectile damage by 3
 macro divprojdmg3()
 	lda $0C2C,X
@@ -40,6 +27,23 @@ macro divprojdmg3()
 	sta $0C2C,X
 endmacro
 
+;;; goes to charge branch whatever items
+org $90b81e
+	bit #$0000
+	bra $0a
+
+;;; disables a "no charge" check
+org $90b8f2
+	bra $00
+
+;;; hijack for beam damage modification 
+org $90b9e6
+	jsr charge
+
+;;; hijack for SBA ammo spend
+org $90ccd2
+	jmp sba_ammo
+
 ;;; nerfed charge : damage modification
 org $90f6a0
 charge:
@@ -52,20 +56,51 @@ charge:
 	lda $0C18,X
 	rts
 
-;;; nerf SBA damage
-org $9381b9
-	jsr sba
+org $90f810
+nochargesba:
+; This alternate table is just as inefficient as the original
+        dw $0000 ; 0: Power
+        dw $0003 ; 1: Wave
+        dw $0003 ; 2: Ice
+        dw $0000 ; 3: Ice + wave
+        dw $0003 ; 4: Spazer
+        dw $0000 ; 5: Spazer + wave
+        dw $0000 ; 6: Spazer + ice
+        dw $0000 ; 7: Spazer + ice + wave
+        dw $0003 ; 8: Plasma
+        dw $0000 ; 9: Plasma + wave
+        dw $0000 ; Ah: Plasma + ice
+        dw $0000 ; Bh: Plasma + ice + wave
+sba_ammo:
+	lda $09a6
+	bit #$1000 : beq .nocharge  ; if charge :
+	lda $09ce    		    ; 	restore A (PB count)
+	jmp $ccd5		    ;   continue original routine
+.nocharge:
+	;; vanilla code actually works only with 1s or 0s in the table
+	;; so we add an actual check for PB qty
+	lda $09ce		    ; PB count
+	sec : sbc nochargesba,X	    ; substract PB qty needed
+	bmi .notenough		    ; if enough PBs:
+	jmp $ccd9		    ; 	proceed with routine after original substraction
+.notenough:			    ; else:
+	jmp $ccc8		    ;   make original function return with carry clear (SBA failed)
 
-org $93f620
-sba:
-	sta $0C2C,X
-	lda $09A6		; equipped beams
-	bit #$1000		; check for charge
-	bne .end
-	;; if no charge, nerfs SBA dmg : divide by 3
-	%divprojdmg3()
-.end:
-	rts
+;;; what's below works, uncomment to enable nerfed SBA damage
+;; ;;; nerf SBA damage
+;; org $9381b9
+;; 	jsr sba_nerf
+
+;; org $93f620
+;; sba_nerf:
+;; 	sta $0C2C,X
+;; 	lda $09A6		; equipped beams
+;; 	bit #$1000		; check for charge
+;; 	bne .end
+;; 	;; if no charge, nerfs SBA dmg : divide by 3
+;; 	%divprojdmg3()
+;; .end:
+;; 	rts
 
 ;;; nerf pseudo screw damage
 org $a0a4cc

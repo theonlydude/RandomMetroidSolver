649276900605c108d9a5a71998fa301966bb6b43
flo <flo@bite.ta>
2020-01-08 23:39:20 +0100

    tools: added ASM to IPS script

---
+++ tools/make_ips.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import sys, os
+
+# now that we're in directory 'tools/' we have to update sys.path
+sys.path.append(os.path.dirname(sys.path[0]))
+
+from ips import IPS_Patch
+
+original=sys.argv[1]
+patched=sys.argv[2]
+target=sys.argv[3]
+
+patch = IPS_Patch.create(open(original, 'rb').read(), open(patched, 'rb').read())
+
+patch.save(target)
+

b924e8d0a9265ccfbdf98b5c518969640f90c464
theonlydude <theonlydude@server.fake>
2020-02-19 18:26:23 +0100

    web: add a randomizer webservice client.

---
+++ randomizer_webservice.py
@@ -0,0 +1,150 @@
+#!/usr/bin/python3
+# call the remote webservice in cli mode instead of generating the seed localy.
+# three parameters are mandatory:
+#  --skillPreset: the skill preset file (~/RandomMetroidSolver/standard_presets/*.json)
+#  --randoPreset: the rando preset file (~/RandomMetroidSolver/rando_presets/*.json)
+#  --rom: the vanilla ROM file
+# for your tests please use your local web2py installation (--remoteUrl local) or the beta website (--remoteUrl beta).
+
+import json, argparse, sys, random, os, os.path, base64, shutil, tempfile
+from requests import Session
+from rom import RealROM
+from ips import IPS_Patch
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Random Metroid Randomizer webservice client")
+    parser.add_argument('--skillPreset', help="skill preset file", dest='skillPreset', nargs='?', default=None)
+    parser.add_argument('--rom', help="vanilla ROM file", dest='rom', nargs='?', default=None)
+    parser.add_argument('--randoPreset', help="rando preset file", dest="randoPreset", nargs='?', default=None)
+    parser.add_argument('--seed', help="seed number (optional)", dest="seed", nargs='?', default=0, type=int)
+    parser.add_argument('--remoteUrl', help="remote url to connect to", dest="remoteUrl", nargs='?', default='local', choices=['local', 'beta', 'production'])
+
+    # parse args
+    args = parser.parse_args()
+
+    if args.skillPreset == None or args.rom == None or args.randoPreset == None:
+        parser.print_help()
+        sys.exit(-1)
+
+    # load rando preset
+    with open(args.randoPreset) as randoPresetFile:
+        randoParams = json.load(randoPresetFile)
+
+    # load skill preset (path/to/file/preset.json)
+    (preset, ext) = os.path.splitext(os.path.split(args.skillPreset)[-1])
+
+    # we send the content of the skill preset instead of its name (for historical reasons)
+    with open(args.skillPreset) as skillPresetFile:
+        randoParams["paramsFileTarget"] = skillPresetFile.read()
+
+    randoParams["preset"] = preset
+    if args.seed == 0:
+        randoParams["seed"] = random.randint(0, 9999999)
+    else:
+        randoParams["seed"] = args.seed
+
+    # not all parameters are present in rando preset, add default value for missing ones
+    defaultParams = {}
+    defaultParams['complexity'] = "simple"
+    defaultParams['preset'] = 'regular'
+    defaultParams['randoPreset'] = ""
+    defaultParams['majorsSplit'] = "Full"
+    defaultParams['startLocation'] = "Landing Site"
+    defaultParams['maxDifficulty'] = 'hardcore'
+    defaultParams['progressionSpeed'] = "medium"
+    defaultParams['progressionDifficulty'] = 'normal'
+    defaultParams['morphPlacement'] = "early"
+    defaultParams['suitsRestriction'] = "on"
+    defaultParams['hideItems'] = "off"
+    defaultParams['strictMinors'] = "off"
+    defaultParams['missileQty'] = "3"
+    defaultParams['superQty'] = "2"
+    defaultParams['powerBombQty'] = "1"
+    defaultParams['minorQty'] = "100"
+    defaultParams['energyQty'] = "vanilla"
+    defaultParams['areaRandomization'] = "off"
+    defaultParams['areaLayout'] = "off"
+    defaultParams['escapeRando'] = "off"
+    defaultParams['removeEscapeEnemies'] = "off"
+    defaultParams['bossRandomization'] = "off"
+    defaultParams['funCombat'] = "off"
+    defaultParams['funMovement'] = "off"
+    defaultParams['funSuits'] = "off"
+    defaultParams['layoutPatches'] = "on"
+    defaultParams['variaTweaks'] = "on"
+    defaultParams['gravityBehaviour'] = "Balanced"
+    defaultParams['nerfedCharge'] = "off"
+    defaultParams['itemsounds'] = "on"
+    defaultParams['elevators_doors_speed'] = "on"
+    defaultParams['spinjumprestart'] = "off"
+    defaultParams['rando_speed'] = "off"
+    defaultParams['animals'] = "off"
+    defaultParams['No_Music'] = "off"
+
+    for (key, value) in defaultParams.items():
+        if key not in randoParams:
+            randoParams[key] = value
+
+    # call web service
+    if args.remoteUrl == 'local':
+        baseUrl = 'http://127.0.0.1:8000/'
+    elif args.remoteUrl == 'beta':
+        baseUrl = 'http://variabeta.pythonanywhere.com/'
+    elif args.remoteUrl == 'production':
+        baseUrl = 'http://randommetroidsolver.pythonanywhere.com/'
+    randoUrl = baseUrl + 'randomizer'
+    webServUrl = baseUrl + 'randomizerWebService'
+
+    # HEAD requests ask for *just* the headers, which is all we need to grab the session cookie
+    session = Session()
+    session.head(randoUrl)
+
+    response = session.post(
+        url=webServUrl,
+        data=randoParams,
+        headers={
+            'Referer': randoUrl
+        }
+    )
+
+    # check that we don't have errors
+    if response.status_code != 200:
+        print("An error {} occured when calling the webservice. Error: {}".format(response.status_code, response.content))
+        exit(-1)
+
+    # the output is a dict
+    data = eval(response.text)
+
+    # generate randomized rom
+    romFileName = args.rom
+    outFileName = data["fileName"]
+    shutil.copyfile(romFileName, outFileName)
+    romFile = RealROM(outFileName)
+
+    # if an ips is provided, apply it
+    if "ips" in data:
+        ipsData = data["ips"]
+        ipsData = base64.b64decode(ipsData.encode('ascii'))
+
+        # our ips patcher need a file (or a dict), not the content of the ips file
+        (fd, ipsFileName) = tempfile.mkstemp()
+        os.close(fd)
+
+        with open(ipsFileName, 'wb+') as ipsFile:
+            ipsFile.write(ipsData)
+
+        romFile.ipsPatch([IPS_Patch.load(ipsFileName)])
+
+        os.remove(ipsFileName)
+        
+    # loop on (address, value) to modify the rom
+    for (key, value) in data.items():
+        if key == "fileName" or key == "errorMsg" or key == "ips" or key == "max_size" or key == "truncate_length":
+            continue
+        romFile.writeByte(value, address=int(key))
+
+    # write dest rom
+    romFile.close()
+
+    print(data["errorMsg"])
+    print("{} generated succesfully".format(outFileName))

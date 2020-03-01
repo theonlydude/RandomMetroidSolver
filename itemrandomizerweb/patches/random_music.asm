a6b4df718b12394d542a3660c73ca0cdd269161e
flo <flo@bite.ta>
2020-03-01 18:03:46 +0100

    asm: fixed/improved random music

--- itemrandomizerweb/patches/random_music.asm
+++ itemrandomizerweb/patches/random_music.asm
@@ -1,5 +1,10 @@
-;;; Randomize music
+;;; Randomize music in game
 ;;;
+;;; Music will change only if music requested by the game is different from last
+;;; requested music.
+;;;
+;;; Uses RNG routine but does not alter current RNG number.
+;;; Uses tracking patch RTA timer to seed RNG.
 ;;;
 ;;; compile with asar (https://www.smwcentral.net/?a=details&id=14560&p=section),
 ;;; or a variant of xkas that supports arch directive
@@ -7,62 +12,48 @@
 lorom
 arch snes.cpu
 
+;;; CONSTANTS
+!nb_tracks	= #$000c
+!nb_tracks_8b	= #$0c
+!last_music_rq	= $7fff30	; RAM address to store music requests by the game
+!last_music_rnd = $7fff32	; RAM address to store our random music
+!room_music	= $07cb
+!room_track	= $07c9
+!current_music	= $07f3
+!current_track	= $07f5
+!RNG		= $808111	; RNG function
+!RNG_seed	= $05e5
+!RTA_timer	= $05b8		; see tracking.asm
+
 ;;; HIJACKS
-org $808FD8
-	;; use a jml to avoid touching the stack
-	jml music_queue
-
-org $809006
-	jsl music_queue_timer
-
-org $82E102
-	jml load_new_music
+org $82DF3E
+	jml load_room_music	; hijack room state header load to replace music/track
 
 org $a98810
 	rep 4 : nop		; disables MB2 "no music" before fight, as cutscene is sped up
 
 org $A1F300
 musics_list:
-	;; do not change order below, the second table is dependent on it
 	;; music set index
-	dw $ff09	; Lower Crateria
-	dw $ff0c	; Upper Crateria PBs
-	dw $ff0f	; Green Brinstar
-	dw $ff12	; Red Brinstar
-	dw $ff15	; Upper Norfair
-	dw $ff1b	; Maridia 1
-	dw $ff1b	; Maridia 2
-	dw $ff30	; Wrecked Ship 1
-	dw $ff30	; Wrecked Ship 2
-	dw $ff18	; Lower Norfair
-	dw $ff36	; Intro
-	dw $ff1b	; Maridia 2
-	dw $ff18	; Lower Norfair
-	dw $ff12	; Red Brinstar
-	dw $ff0f	; Green Brinstar
-	dw $ff1e	; Tourian
-
-number_tracks:
-	;; number of possible music tracks
-	db $01	; Lower Crateria
-	db $01	; Upper Crateria PBs
-	db $01	; Green Brinstar
-	db $01	; Red Brinstar
-	db $01	; Upper Norfair
-	db $02	; Maridia 1
-	db $02	; Maridia 2
-	db $02	; Wrecked Ship 1
-	db $02	; Wrecked Ship 2
-	db $01	; Lower Norfair
-	db $01	; Intro
-	db $02	; Maridia 2
-	db $01	; Lower Norfair
-	db $01	; Red Brinstar
-	db $01	; Green Brinstar
-	db $01	; Tourian
+	;; hi: music data index
+	;; lo: music track index
+	dw $0905	; Lower Crateria
+	dw $0c05	; Upper Crateria PBs
+	dw $0f05	; Green Brinstar
+	dw $1205	; Red Brinstar
+	dw $1505	; Upper Norfair
+	dw $1b05	; Maridia 1
+	dw $1b06	; Maridia 2
+	dw $3005	; Wrecked Ship 1
+	dw $3006	; Wrecked Ship 2
+	dw $1805	; Lower Norfair
+	dw $3605	; Intro
+	dw $1e05	; Tourian
 
 ;;; check that music in A is in musics_list table.
 ;;; set carry flag if true.
+;;; if carry flag is set, X holds index in musics_list
+;;; doesn't touch A
 is_music_to_randomize:
 	;; set X to 0
 	ldx #$0000
@@ -71,7 +62,7 @@ is_music_to_randomize:
 	cmp.l musics_list,x
 	beq .music_is_random
 	inx : inx
-	cpx #$0020 : beq .music_is_not_random : bra .loop
+	cpx !nb_tracks : beq .music_is_not_random : bra .loop
 .music_is_not_random:
 	;; clear carry flag
 	clc
@@ -79,94 +70,112 @@ is_music_to_randomize:
 .music_is_random:
 	;; set carry flag
 	sec
-.endloop:	
+.endloop:
 	rts
 
-music_queue:
-;;; $80:8FD8 68          PLA                
-;;; $80:8FD9 EC 3B 06    CPX $063B  [$7E:063B]
-	pla			; original code
-	jsr randomize_music_and_track
-	cpx $063b		; original code
-	jml $808fdc		; called with jml
-	
-music_queue_timer:
-;;; $80:9006 9D 19 06    STA $0619,x[$7E:0619]
-;;; $80:9009 98          TYA
-	phy			; save Y
-	cmp #$0000		; sets cpu flags wrt A value
-	jsr randomize_music_and_track
-	ply			; restore Y
-	sta $0619,x		; original code
-	tya			; original code
-	rtl
+;;; single use random function that leaves game rng untouched
+rand:
+	lda !RNG_seed : pha
+	eor !RTA_timer : sta !RNG_seed
+	jsl !RNG : pha
+	pla : sta !RNG_seed
+	pla
+	rts
 
-load_new_music:
-;;; $82:E102 C5 14       CMP $14    [$7E:0014]
-;;; $82:E104 F0 0A       BEQ $0A    [$E110]
-	;; if new music, load it
-	;; if same music, check if the new one is a random one, if it is, load it anyway
-	cmp $14 		; original code
-	beq .same_music
-	jml $82E106		; go back to music changing code
-.same_music:
-	pha			; save A
-	lda $07F3 : ora #$FF00	; set music xx in A: 0xFFxx
-	jsr is_music_to_randomize
-	pla			; restore A
-	bcc .notrandom
-	jml $82E106		; go to the part of the hijacked function which load the new music
-.notrandom:
-	jml $82E110		; go to the end of the hijacked function
+;;; gets a random data/track couple from table, and store it in last_music_rnd (and A)
+get_random_music:
+	;; call RNG, result in A
+	jsr rand
+	;; A = A % nb_tracks
+	sta $4204
+	;; switch to 8-bit mode for divisor
+	sep #$20
+	lda !nb_tracks_8b : sta $4206
+	pha : pla : xba : xba 	; wait for division
+	;; back to 16-bit mode
+	rep #$20
+	lda $4216		; get remainder to have modulo
+	;; load random data/track couple in A
+	asl : tax : lda.l musics_list,x
+	sta !last_music_rnd
+	rts
 
-randomize_music_and_track:
-	;; pla has set the n flag in calling functions.
-	;; music data has its first byte as 0xFF (negative)
+;;; hijacks room music loading. the music data and track index in room header
+;;; will be randomized if:
+;;; - new music is actually requested
+;;; - this new music is different from the last new music we tried to load
+;;; xxtt with tt 05 or 06 and xx not 0 => if music combination is to be randomized,
+;;; 		                          randomize and use it.
+;;; 					  if not store as asked.
+;;; 					  in all cases, store asked music
+;;; xxtt with tt not 05 or 06 => if xx is 0, store it as asked.
+;;;                              if not, check if xx05 or xx06 matches last asked music.
+;;;                              if it does not, randomize music but just store it
+;;;                              in last random music.
+;;; 				 if it does, store as asked
+;;; 0005/0006 => if last asked song was randomized, restore random music
+;;; 		 if not, restore last asked music
+load_room_music:
 	phx
-	bmi .check_music
-	bra .check_track
-.check_music:
-	jsr is_music_to_randomize
-	bcc .end
-.randomize_music:
-	;; call RNG, result in A
-	jsl $808111
-	;; A = A % 16
-	and #$000F
-	;; X = A*2
-	asl : tax
-	;; load in A value of musics_list[x]
-	lda.l musics_list,x
-	bra .end
-.check_track:
+	;; at this point, A contains music data index in lo byte, 0 in hi
+	;; X contains room header index in bank 8F (DB=8F)
+	xba
+	sep #$20
+	lda $0005,x
+	;; A now contains music data in hi byte and track in lo byte
+	;; (from room header)
+	cmp #$05	: beq .track_ok
+	cmp #$06	: beq .track_ok
+	rep #$20
+	bit #$ff00	: beq .store_music	; vanilla load if 00xx
+	;; here, music data is != 0, but track is neither 05 nor 06
+	;; check if data|05 or data|06 matches last music request
 	pha
-	lda $063d     ; load current music
-	jsr is_music_to_randomize
-	pla			; pla doesn't change the carry flag
-	bcc .end
-	;; check if track is 0x05 or 0x06 (track1 or track2)
-	cmp #$0005 : beq .track_ok
-	cmp #$0006 : beq .track_ok
-	bra .end
+	and #$ff00 : ora #$0005
+	cmp !last_music_rq : beq .restore_music
+	and #$ff00 : ora #$0006
+	cmp !last_music_rq : beq .restore_music
+	;; randomize music but don't use it, just store it in last_music_rnd
+	jsr get_random_music
+	pla : pha
+	and #$ff00 : ora #$0005
+	sta !last_music_rq	; store data|05 as last requested
+.restore_music:
+	pla
+	bra .store_music
 .track_ok:
-	;; load number of tracks using X/2 (X was set in is_music_to_randomize)
-	txa : lsr : tax
-	lda.l number_tracks,x
-	and #$00ff
-	cmp #$0001 : bne .randomize_track
-	;;  if only one track, use it (track1 = 0x05)
-	lda #$0005
-	bra .end
-.randomize_track:
-	;; two tracks, randomize between them (track1=0x05, track2=0x06)
-	;; call RNG, result in A
-	jsl $808111
-	;; A = A % 2
-	and #$0001
-	;; A = A + 0x05
-	clc : adc #$0005
+	;; if music data = 0 but track is 5 or 6, load last music (randomized or not)
+	xba : beq .dont_change	; still in 8 bits mode here
+	xba
+	;; check if music/track couple is to be randomized
+	rep #$20
+	jsr is_music_to_randomize : bcc .dont_randomize
+	cmp !last_music_rq	  : bne .randomize
+	;; we're here when new music is asked but it's the same
+	;; as the last asked new music.
+.dont_change:
+	;; restore last randomized music if last asked music was randomized
+	;; if not, restore last asked music
+	rep #$20
+	lda !last_music_rq
+	jsr is_music_to_randomize : bcc .store_music
+	lda !last_music_rnd	  : bra .store_music
+.dont_randomize:
+	sta !last_music_rq
+	bra .store_music
+.randomize:
+	sta !last_music_rq
+	jsr get_random_music
+.store_music:
+	;; stores A word as defined in musics_list is RAM room header
+	sep #$20
+	sta !room_track
+	xba
+	sta !room_music
+	rep #$20
 .end:
+	;; resume original routine after music/track load
 	plx
-	rts
+	jml $82DF4A
 
 warnpc $a1f3ff

c93e5dd5a757203cb2d90f36660df0ab7fc37dbc
flo <flo@bite.ta>
2020-04-03 22:47:59 +0200

    logic: adapted lava dive etanks requirements to actual damage reduction instead of varia

--- graph_helpers.py
+++ graph_helpers.py
@@ -274,11 +274,9 @@ class HelpersGraph(Helpers):
     @Cache.decorator
     def canPassLavaPit(self):
         sm = self.smbm
-        nTanks4Dive = 3
-        if sm.heatProof().bool == False:
-            nTanks4Dive = 8
+        nTanks4Dive = 8 / sm.getDmgReduction()[0]
         if sm.haveItem('HiJump').bool == False:
-            nTanks4Dive = ceil(nTanks4Dive * 1.25) # 4 or 10
+            nTanks4Dive = ceil(nTanks4Dive * 1.25)
         return sm.wand(sm.wor(sm.wand(sm.haveItem('Gravity'), sm.haveItem('SpaceJump')),
                               sm.wand(sm.knowsGravityJump(), sm.haveItem('Gravity'), sm.wor(sm.haveItem('HiJump'), sm.knowsLavaDive())),
                               sm.wand(sm.wor(sm.wand(sm.knowsLavaDive(), sm.haveItem('HiJump')),

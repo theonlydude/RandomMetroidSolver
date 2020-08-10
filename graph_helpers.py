theonlydude/RandomMetroidSolver

commit c1082a7f4582bb847414e72783c3ecae7950c7c1

logic: fix on missile below ice from croc

graph_helpers.py
@@ -572,14 +572,15 @@ class HelpersGraph(Helpers):
                       sm.wand(sm.canPassBombPassages(), # wjs and/or 3 tile mid air morph
                               sm.knowsMaridiaWallJumps()))
 
-    def getDraygonConnection(self):
-        if self.draygonConnection is None:
-            drayRoomOut = getAccessPoint('DraygonRoomOut')
-            self.draygonConnection = drayRoomOut.ConnectedTo
-        return self.draygonConnection
-
+    @Cache.decorator
     def isVanillaDraygon(self):
-        return self.getDraygonConnection() == 'DraygonRoomIn'
+        drayRoomOut = getAccessPoint('DraygonRoomOut')
+        return SMBool(drayRoomOut.ConnectedTo == 'DraygonRoomIn')
+
+    @Cache.decorator
+    def isVanillaCroc(self):
+        crocRoom = getAccessPoint('Crocomire Room Top')
+        return SMBool(crocRoom.ConnectedTo == 'Crocomire Speedway Bottom')
 
     @Cache.decorator
     def canFightDraygon(self):

graph_locations.py
@@ -1338,12 +1338,13 @@ locations = [
     'Room': 'Crumble Shaft',
     'AccessFrom' : {
         'Business Center': lambda sm: sm.wand(sm.canOpenGreenDoors(),
-                                                      sm.canUsePowerBombs(),
-                                                      sm.canHellRun(**Settings.hellRunsTable['Ice']['Norfair Entrance -> Ice Beam']),
-                                                      sm.wor(sm.wand(sm.haveItem('Morph'),
-                                                                     sm.knowsMockball()),
-                                                             sm.haveItem('SpeedBooster'))),
-        'Crocomire Speedway Bottom': lambda sm: sm.wand(sm.canHellRun(**Settings.hellRunsTable['MainUpperNorfair']['Croc -> Ice Missiles']),
+                                              sm.canUsePowerBombs(),
+                                              sm.canHellRun(**Settings.hellRunsTable['Ice']['Norfair Entrance -> Ice Beam']),
+                                              sm.wor(sm.wand(sm.haveItem('Morph'),
+                                                             sm.knowsMockball()),
+                                                     sm.haveItem('SpeedBooster'))),
+        'Crocomire Speedway Bottom': lambda sm: sm.wand(sm.isVanillaCroc(),
+                                                        sm.canHellRun(**Settings.hellRunsTable['MainUpperNorfair']['Croc -> Ice Missiles']),
                                                         sm.haveItem('SpeedBooster'),
                                                         sm.knowsIceMissileFromCroc())
     },

commit b49acb3cdbad3442664ea7ddb02c57b311c5b756

logic: fix regression introduced by commit c1082a7f4582bb847414e72783c3ecae7950c7c1

graph_helpers.py
@@ -9,7 +9,6 @@ from parameters import Settings
 class HelpersGraph(Helpers):
     def __init__(self, smbm):
         self.smbm = smbm
-        self.draygonConnection = None
 
     def canEnterAndLeaveGauntletQty(self, nPB, nTanksSpark):
         sm = self.smbm
@@ -573,9 +572,12 @@ class HelpersGraph(Helpers):
                               sm.knowsMaridiaWallJumps()))
 
     @Cache.decorator
+    def getDraygonConnection(self):
+        return getAccessPoint('DraygonRoomOut').ConnectedTo
+
+    @Cache.decorator
     def isVanillaDraygon(self):
-        drayRoomOut = getAccessPoint('DraygonRoomOut')
-        return SMBool(drayRoomOut.ConnectedTo == 'DraygonRoomIn')
+        return SMBool(self.getDraygonConnection() == 'DraygonRoomIn')
 
     @Cache.decorator
     def isVanillaCroc(self):
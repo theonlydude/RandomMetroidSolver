87431fd217b166cc15e0c839b87b8faf56c2c282
flo <flo@bite.ta>
2020-09-18 20:04:20 +0200

    rando: fixed persisent super fun setting disable during layout retries

--- rando/RandoSettings.py
+++ rando/RandoSettings.py
@@ -22,6 +22,12 @@ class RandoSettings(object):
         self.plandoRandoItemLocs = plandoRandoItemLocs
         self.minDiff = minDiff
 
+    def getSuperFun(self):
+        return self.superFun[:]
+
+    def updateSuperFun(self, superFun):
+        self.superFun = superFun[:]
+
     def isPlandoRando(self):
         return self.plandoRandoItemLocs is not None
 

8022b4b29f4dc6efc06986ae7b5de854a436ef49
Paul Brannan <cout@users.noreply.github.com>
2020-09-20 20:39:22 -0400

    cache: enable debugging with a flag
    Added a flag Cache.debug which will enable/disable cache debugging
    output.  There is a very slight performance hit, in exchange for
    readability, which will be resolved in a future commit.
    
    Cache debug output is exactly the same as prior to this change.

--- cache.py
+++ cache.py
@@ -1,51 +1,93 @@
 # the caching decorator for helpers functions
 
+class NotFound: pass
+
+class Debug_MasterCache(dict):
+    def __contains__(self, newKey):
+        has_newKey = dict.__contains__(self, newKey)
+        if has_newKey:
+            print("usek: "+format(newKey, '#067b'))
+            if Cache.masterCache[newKey]['key'] != newKey:
+                print("ERROR: key stored in cache is not the same as the new key !!!!")
+                print("      "+format(self[newKey]['key'], '#067b')+" key in cache")
+        return has_newKey
+
+    def __setitem__(self, newKey, value):
+        dict.__setitem__(self, newKey, value)
+        if newKey != 0:
+            print("newk: "+format(newKey, '#067b'))
+
+class Debug_Cache(dict):
+    def get(self, name, default):
+        ret = dict.get(self, name, NotFound)
+        if ret is not NotFound:
+            if name in Cache.funcs:
+                print("cache found for {}: {}".format(name, self[name]))
+            return ret
+        else:
+            return default
+
+    def __setitem__(self, name, value):
+        dict.__setitem__(self, name, value)
+        if name != 'key' and name in Cache.funcs:
+            print("cache added for {}: {}".format(name, self[name]))
+
+    def validate(self, name, ret):
+        if ret != self[name] and name in Cache.funcs:
+            print("ERROR: cache ({}) != current ({}) for {}".format(self[name], ret, name))
+
 class Cache:
     cache = {}
     masterCache = {}
+    funcs = set()
+    lambdas = set()
+    debug = False
 
     @staticmethod
     def reset():
         # reinit the whole cache
         key = 0
-        Cache.masterCache = {}
-        Cache.cache = {'key': key}
+
+        if Cache.debug:
+            Cache.masterCache = Debug_MasterCache()
+            Cache.cache = Debug_Cache()
+        else:
+            Cache.masterCache = { }
+            Cache.cache = { }
+
+        Cache.cache['key'] = key
         Cache.masterCache[key] = Cache.cache
 
     @staticmethod
     def update(newKey):
         if newKey in Cache.masterCache:
-#            print("usek: "+format(newKey, '#067b'))
-#            if Cache.masterCache[newKey]['key'] != newKey:
-#                print("ERROR: key stored in cache is not the same as the new key !!!!")
-#                print("      "+format(Cache.masterCache[newKey]['key'], '#067b')+" key in cache")
             Cache.cache = Cache.masterCache[newKey]
         else:
-#            print("newk: "+format(newKey, '#067b'))
-            Cache.cache = {'key': newKey}
+            Cache.cache = Debug_Cache() if Cache.debug else { }
+            Cache.cache['key'] = newKey
             Cache.masterCache[newKey] = Cache.cache
 
     @staticmethod
     def decorator(func):
         name = func.__name__
+        Cache.funcs.add(name)
         def _decorator(self):
             ret = Cache.cache.get(name, None)
             if ret is not None:
-#                print("cache found for {}: {}".format(name, Cache.cache[func.__name__]))
-#                ret = func(self)
-#                if ret != Cache.cache[name]:
-#                    print("ERROR: cache ({}) != current ({}) for {}".format(Cache.cache[name], ret, name))
+                if Cache.debug:
+                    ret = func(self)
+                    Cache.cache.validate(name, ret)
                 return ret
             else:
                 ret = func(self)
                 Cache.cache[name] = ret
-#                print("cache added for {}: {}".format(name, Cache.cache[name]))
                 return ret
         return _decorator
 
     # for lambdas
     @staticmethod
     def ldeco(name, func):
+        Cache.lambdas.add(name)
         def _decorator(self):
             ret = Cache.cache.get(name, None)
             if ret is not None:

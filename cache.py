b86fac8652ce86d1f57b5b031e548a0020dd4def
Paul Brannan <cout@users.noreply.github.com>
2020-09-23 07:00:38 -0400

    cache: Use list instead of dict
    Changed the cache to use a list instead of a dict for storing the
    results of functions.  This eliminates a dictionary lookup, as each
    decorator gets its own slot at a specific index in the list, at the cost
    of having to construct the list each time we change the cache key.  This
    provides a small but measurable speed improvement over using a dict.

--- cache.py
+++ cache.py
@@ -7,9 +7,9 @@ class Debug_MasterCache(dict):
         ret = dict.get(self, newKey, NotFound)
         if ret is not NotFound:
             print("usek: "+format(newKey, '#067b'))
-            if Cache.masterCache[newKey]['key'] != newKey:
+            if Cache.masterCache[newKey].key != newKey:
                 print("ERROR: key stored in cache is not the same as the new key !!!!")
-                print("      "+format(self[newKey]['key'], '#067b')+" key in cache")
+                print("      "+format(self[newKey].key, '#067b')+" key in cache")
             return ret
         else:
             return default
@@ -19,35 +19,44 @@ class Debug_MasterCache(dict):
         if newKey != 0:
             print("newk: "+format(newKey, '#067b'))
 
-class Debug_Cache(dict):
-    def get(self, name, default):
-        ret = dict.get(self, name, NotFound)
-        if ret is not NotFound:
-            if name in Cache.funcs:
-                print("cache found for {}: {}".format(name, self[name]))
+class Debug_Cache(list):
+    def __init__(self, key, size):
+        list.__init__(self, [ None ] * size)
+        self.key = key
+
+    def __getitem__(self, slot):
+        ret = list.__getitem__(self, slot)
+        if ret is not None:
+            if slot in Cache.debug_slots:
+                name = Cache.debug_slots[slot]
+                print("cache found for {}: {}".format(name, ret))
             return ret
         else:
-            return default
+            return None
 
-    def __setitem__(self, name, value):
-        dict.__setitem__(self, name, value)
-        if name != 'key' and name in Cache.funcs:
-            print("cache added for {}: {}".format(name, self[name]))
+    def __setitem__(self, slot, value):
+        list.__setitem__(self, slot, value)
+        if slot in Cache.debug_slots:
+            name = Cache.debug_slots[slot]
+            print("cache added for {}: {}".format(name, value))
 
-    def validate(self, name, ret):
-        if ret != self[name]:
-            print("ERROR: cache ({}) != current ({}) for {}".format(self[name], ret, name))
+    def validate(self, name, slot, ret):
+        value_in_cache = list.__getitem__(self, slot)
+        if ret != value_in_cache:
+            print("ERROR: cache ({}) != current ({}) for {}".format(value_in_cache, ret, name))
 
 class VersionedCache(object):
-    __slots__ = ( 'cache', 'masterCache', 'funcs', 'lambdas', 'debug', 'key' )
+    __slots__ = ( 'cache', 'masterCache', 'debug', 'key', 'next_slot',
+    'debug_slots', 'size' )
 
     def __init__(self):
-        self.cache = {}
+        self.cache = []
         self.masterCache = {}
-        self.funcs = set()
-        self.lambdas = set()
         self.debug = False
         self.key = None
+        self.next_slot = 0
+        self.debug_slots = { }
+        self.size = 0
 
     def reset(self):
         # reinit the whole cache
@@ -59,7 +68,7 @@ class VersionedCache(object):
             return
         cache = self.masterCache.get(newKey, None)
         if cache is None:
-            cache = Debug_Cache(key=newKey) if self.debug else { }
+            cache = Debug_Cache(newKey, self.size) if self.debug else [ None ] * self.size
             self.masterCache[newKey] = cache
             self.cache = cache
             self.key = newKey
@@ -69,28 +78,35 @@ class VersionedCache(object):
 
     def decorator(self, func):
         name = func.__name__
-        self.funcs.add(name)
-        return self._decorate(name, func)
+        slot = self._new_slot(name)
+        self.debug_slots[slot] = name
+        return self._decorate(name, slot, func)
 
     # for lambdas
     def ldeco(self, name, func):
-        self.lambdas.add(name)
-        return self._decorate(name, func)
+        slot = self._new_slot(name)
+        return self._decorate(name, slot, func)
 
-    def _decorate(self, name, func):
+    def _new_slot(self, name):
+        slot = self.next_slot
+        self.next_slot += 1
+        self.size += 1
+        return slot
+
+    def _decorate(self, name, slot, func):
         def _decorator(arg):
-            ret = self.cache.get(name, None)
+            ret = self.cache[slot]
             if ret is not None:
                 # TODO: Commented-out, because this function can get
                 # called 500K times or more during a run, and so this
                 # conditional measurably affects performance.
-                # if self.debug and name in self.funcs:
+                # if self.debug and slot in self.debug_slots:
                 #     ret = func(arg)
-                #     self.cache.validate(name, ret)
+                #     self.cache.validate(name, slot, ret)
                 return ret
             else:
                 ret = func(arg)
-                self.cache[name] = ret
+                self.cache[slot] = ret
                 return ret
         return _decorator
 

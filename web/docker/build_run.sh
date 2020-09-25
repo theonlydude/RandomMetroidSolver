4879c2055620b1c501531f2a7c42daf8dba5faa6
theonlydude <theonlydude@server.fake>
2020-09-25 18:55:48 +0200

    docker: use github token to git clone in the docker image.

--- web/docker/build_run.sh
+++ web/docker/build_run.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 # build and run mysql & varia docker images
-# parameter1: the branch to checkout on varia git repo
-# parameter2: optional dump to import in db
+# ./build_run.sh -b master [-d path/to/dump.sql] [-t github_token]
+# -b branch: optional  branch to checkout on varia git repo (default to master)
+# -d dump: optional dump to import in db to populate it with data
+# -s github_token: optional token to git clone and git pull.
+#                  if not provided download the repo as a zip.
 
 # cd to root dir
 CWD=$(dirname $0)
 cd ${CWD}
 CWD=$(pwd)
 
-BRANCH="${1:-master}"
-DUMP="${2}"
+BRANCH="master"
+DUMP=""
+GITHUB_TOKEN=""
+while getopts "b:d:t:" ARG; do
+    case ${ARG} in
+        b) export BRANCH="${OPTARG}";;
+        d) export DUMP="${OPTARG}";;
+        t) export GITHUB_TOKEN="${OPTARG}";;
+	*) echo "Unknown option ${ARG}"; exit 0;;
+    esac
+done
 
 # mysql init files
 if [ -z "${DUMP}" ]; then
@@ -17,17 +29,22 @@ if [ -z "${DUMP}" ]; then
 else
     cp -p ${DUMP} mysql/dump.sql
 fi
+# tables creation
 cp ../database/*.sql mysql/
 
 # mysql image build & run
 docker build --tag varia-mysql --build-arg DUMP="${DUMP}" -f mysql/Dockerfile mysql/ &&
-docker run --name varia-mysql --publish 3366:3306 -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=varia -e MYSQL_USER=varia -e MYSQL_PASSWORD=varia -d varia-mysql &&
+docker run --name varia-mysql --publish 3366:3306 -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=varia -e MYSQL_USER=varia -e MYSQL_PASSWORD=varia -d varia-mysql
 
 # web2py + varia image build & run
-HOSTNAME=127.0.0.1
-docker build --tag varia-${BRANCH} --build-arg BRANCH=${BRANCH} --build-arg HOSTNAME=${HOSTNAME} -f web2py/Dockerfile web2py/ &&
+docker build --tag varia-${BRANCH} --build-arg BRANCH=${BRANCH} --build-arg GITHUB_TOKEN=${GITHUB_TOKEN} -f web2py/Dockerfile web2py/ &&
 docker run -d --name varia-${BRANCH} --network="host" varia-${BRANCH}
+
+rm -f mysql/*.sql
 
 # to check web2py logs:
 # docker exec varia-master ls -lhrt /var/log/supervisor/
 # docker exec varia-master cat /var/log/supervisor/web2py-stdout---supervisor-xxxxxx.log
+
+# to update git repo:
+# docker exec -w /root/RandomMetroidSolver varia-master git pull

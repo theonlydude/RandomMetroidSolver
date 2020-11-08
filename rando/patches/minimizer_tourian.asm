710b8d514a18eefe048c03e0220cbaad9c4bfe7e
flo <flo@bite.ta>
2020-11-08 16:35:22 +0100

    minimizer: fixed tourian door bitflag

--- rando/patches/minimizer_tourian.asm
+++ rando/patches/minimizer_tourian.asm
@@ -4,6 +4,8 @@
 ;;;   beat all Golden Four)
 ;;; - add door ASM for Gadora's door: refill+kill the zebs and MB
 ;;;   glass
+;;; - skips MB3 fight and baby cutscene: MB dies at the end of MB2,
+;;;   and you get hyper+refill for the escape
 ;;;
 ;;; Compiles with asar
 
@@ -23,9 +25,11 @@ lorom
 ;;; connect Statues Hallway to Tourian Eye Door Room...
 org $8fa616
 	dw $aa5c
+	db $40
 ;;; ...and back
 org $8fddeb
 	dw $9216
+	db $40
 
 ;;; alternative door hit instruction that skips hit counter check
 org $848a6d ; end of some unused instruction

6e1f5bb9b724153e8a6f6e628d91e55871eda2b6
theonlydude <theonlydude@server.fake>
2021-08-02 19:27:56 +0200

    version: update version displayed on title screen

--- utils/version.py
+++ utils/version.py
@@ -1,3 +1,3 @@
 # version displayed on the title screen, must be a max 32 chars [a-z0-9.-] string
 # either 'beta' or 'r.yyyy.mm.dd'
-displayedVersion = 'r.2021.02.21'
+displayedVersion = 'r.2021.08.02'

638f1db9af9d3d92fa92a7a7723af0b05280cced
theonlydude <theonlydude@server.fake>
2021-09-06 18:08:19 +0200

    ship: update extraction tool to handle relocated palette

--- tools/extract_ship.py
+++ tools/extract_ship.py
@@ -21,16 +21,25 @@ copyfile(vanilla, tmpfile)
 
 # extract data from hack
 addresses = {
-    "tilesAddr": [snes_to_pc(0xADB600), snes_to_pc(0xADC600)],
-    "palettesAddr": [snes_to_pc(0xA2A59E), snes_to_pc(0xA2A5BE)],
-    "glowSpritemapsInstructionList": [snes_to_pc(0x8dca4e), snes_to_pc(0x8DCAAA)],
-    "spritemapsInstructionList": [snes_to_pc(0xA2A5BE), snes_to_pc(0xA2A622)],
-    "instructionList": [snes_to_pc(0xA2A644), snes_to_pc(0xA2A783)],
-    "spritemaps": [snes_to_pc(0xA2AD81), snes_to_pc(0xA2AFF3)],
-    "enemyHeader": [snes_to_pc(0xA0D07F), snes_to_pc(0xA0D0FF)],
-    "enemySet": [snes_to_pc(0xA1883D), snes_to_pc(0xA18870)],
+    "tilesAddr": {"vanilla": [snes_to_pc(0xADB600), snes_to_pc(0xADC600)],
+                  "hack":    [snes_to_pc(0xADB600), snes_to_pc(0xADC600)]},
+    "palettesAddr": {"vanilla": [snes_to_pc(0xA2A59E), snes_to_pc(0xA2A5BE)],
+                     "hack":    [snes_to_pc(0xA2A59E), snes_to_pc(0xA2A5BE)]},
+    "glowSpritemapsInstructionList": {"vanilla": [snes_to_pc(0x8dca4e), snes_to_pc(0x8DCAAA)],
+                                      "hack":    [snes_to_pc(0x8dca4e), snes_to_pc(0x8DCAAA)]},
+    "spritemapsInstructionList": {"vanilla": [snes_to_pc(0xA2A5BE), snes_to_pc(0xA2A622)],
+                                  "hack":    [snes_to_pc(0xA2A5BE), snes_to_pc(0xA2A622)]},
+    "instructionList": {"vanilla": [snes_to_pc(0xA2A644), snes_to_pc(0xA2A783)],
+                        "hack":    [snes_to_pc(0xA2A644), snes_to_pc(0xA2A783)]},
+    "spritemaps": {"vanilla": [snes_to_pc(0xA2AD81), snes_to_pc(0xA2AFF3)],
+                   "hack":    [snes_to_pc(0xA2AD81), snes_to_pc(0xA2AFF3)]},
+    "enemyHeader": {"vanilla": [snes_to_pc(0xA0D07F), snes_to_pc(0xA0D0FF)],
+                    "hack":    [snes_to_pc(0xA0D07F), snes_to_pc(0xA0D0FF)]},
+    "enemySet": {"vanilla": [snes_to_pc(0xA1883D), snes_to_pc(0xA18870)],
+                 "hack":    [snes_to_pc(0xA1883D), snes_to_pc(0xA18870)]},
     #"enemyGfxName": [snes_to_pc(0xB4818C), snes_to_pc(0xB48193)],
-    "enemyGfx": [snes_to_pc(0xB48193), snes_to_pc(0xB4819E)]
+    "enemyGfx": {"vanilla": [snes_to_pc(0xB48193), snes_to_pc(0xB4819E)],
+                 "hack":    [snes_to_pc(0xB48193), snes_to_pc(0xB4819E)]}
 }
 
 needCopy = {
@@ -48,29 +57,6 @@ needCopy = {
 
 hackRom = RealROM(hack)
 vanillaRom = RealROM(tmpfile)
-
-for name, addrRange in addresses.items():
-    print("check {}".format(name))
-    diffFound = False
-    for addr in range(addrRange[0], addrRange[1]):
-        hByte = hackRom.readByte(addr)
-        vByte = vanillaRom.readByte(addr)
-        if hByte != vByte:
-            print("difference between vanilla and hack detected")
-            diffFound = True
-            break
-    needCopy[name] = diffFound
-    if diffFound and addrRange[1] - addrRange[0] < 128:
-        hBytes = []
-        vBytes = []
-        for addr in range(addrRange[0], addrRange[1]):
-            hBytes.append(hackRom.readByte(addr))
-            vBytes.append(vanillaRom.readByte(addr))
-        print("vanilla: {}".format([hex(b) for b in vBytes]))
-        print("hack:    {}".format([hex(b) for b in hBytes]))
-        
-    print("end check {}".format(name))
-    print("")
 
 # level data are compressed, extract tiles in ship screen based on spritemaps
 landingSiteAddr = snes_to_pc(0x8F91F8)
@@ -114,6 +100,17 @@ if "top" not in hShips or "bottom" not in hShips:
     raise Exception("ship top/bottom not found in hack landing site enemies (using initAi)")
 
 #print("vanilla enemies: {}".format([hex(enemy) for enemy in vEnemies]))
+
+# check if ship has relocated palette
+vanillaPaletteAddr = 0xA59E
+if hShips["top"].palette != vanillaPaletteAddr:
+    addr = snes_to_pc(0xA20000+hShips["top"].palette)
+    addresses["palettesAddr"]["hack"] = [addr, addr+32]
+    print("ship top has relocated palette addr: {}".format(hex(addr)))
+if hShips["bottom"].palette != vanillaPaletteAddr:
+    addr = snes_to_pc(0xA20000+hShips["bottom"].palette)
+    addresses["palettesAddr"]["hack"] = [addr, addr+32]
+    print("ship bottom has relocated palette addr: {}".format(hex(addr)))
 
 vShipScreen = (int(vShips["top"].Xpos/256), int(vShips["top"].Ypos/256))
 hShipScreen = (int(hShips["top"].Xpos/256), int(hShips["top"].Ypos/256))
@@ -159,19 +156,6 @@ print("")
 #hlevelData.displaySubScreen(hShipScreen, hShipBottom.spritemap.boundingRect)
 #print("")
 
-# copy data
-for name, diff in needCopy.items():
-    if not diff:
-        continue
-    print("copy {} from hack".format(name))
-    tmpBytes = []
-    addrs = addresses[name]
-    for addr in range(addrs[0], addrs[1]):
-        tmpBytes.append(hackRom.readByte(addr))
-    vanillaRom.seek(addrs[0])
-    for byte in tmpBytes:
-        vanillaRom.writeByte(byte)
-
 # first empty behind vanilla ship in case hack sprite is smaller
 vlevelData.emptyLayout(vShipScreen, vShipTop.spritemap.boundingRect)
 vlevelData.emptyLayout(vShipScreen, vShipBottom.spritemap.boundingRect)
@@ -189,6 +173,43 @@ vlevelData.displaySubScreen(vShipScreen, hShipBottom.spritemap.boundingRect)
 print("")
 
 vlevelData.write()
+
+for name, addrRange in addresses.items():
+    print("check {} at {}".format(name, hex(addrRange["vanilla"][0])))
+    diffFound = False
+    for addrV, addrH in zip(range(addrRange["vanilla"][0], addrRange["vanilla"][1]), range(addrRange["hack"][0], addrRange["hack"][1])):
+        hByte = hackRom.readByte(addrH)
+        vByte = vanillaRom.readByte(addrV)
+        if hByte != vByte:
+            print("difference between vanilla and hack detected")
+            diffFound = True
+            break
+    if name not in ["enemySet", "enemyHeader"]:
+        needCopy[name] = diffFound
+    if diffFound and addrRange["vanilla"][1] - addrRange["vanilla"][0] < 256:
+        hBytes = []
+        vBytes = []
+        for addrV, addrH in zip(range(addrRange["vanilla"][0], addrRange["vanilla"][1]), range(addrRange["hack"][0], addrRange["hack"][1])):
+            hBytes.append(hackRom.readByte(addrH))
+            vBytes.append(vanillaRom.readByte(addrV))
+        print("vanilla: {}".format([hex(b) for b in vBytes]))
+        print("hack:    {}".format([hex(b) for b in hBytes]))
+
+    print("end check {}".format(name))
+    print("")
+
+# copy data
+for name, diff in needCopy.items():
+    if not diff:
+        continue
+    print("copy {} from hack".format(name))
+    tmpBytes = []
+    addrs = addresses[name]
+    for addr in range(addrs["hack"][0], addrs["hack"][1]):
+        tmpBytes.append(hackRom.readByte(addr))
+    vanillaRom.seek(addrs["vanilla"][0])
+    for byte in tmpBytes:
+        vanillaRom.writeByte(byte)
 
 vanillaRom.close()
 

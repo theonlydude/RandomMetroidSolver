15a3911764278be0a0b3f4badbfad45851b85307
flo <flo@flo.flo>
2022-05-21 19:59:05 +0200

    tools: added dict patches to reverse ips

--- tools/reverse_ips.py
+++ tools/reverse_ips.py
@@ -7,6 +7,7 @@ sys.path.append(os.path.dirname(sys.path[0]))
 
 from rom.ips import IPS_Patch
 from rom.rom import RealROM
+from patches.common import patches as common_patches
 
 vanilla=sys.argv[1]
 ipsToReverse = sys.argv[2:]
@@ -14,12 +15,17 @@ ipsToReverse = sys.argv[2:]
 rom = RealROM(vanilla)
 
 for ips in ipsToReverse:
-    ipsPath = os.path.abspath(ips)
-    destDir, destFile = os.path.split(ipsPath)
+    if ips not in common_patches.patches:
+        ipsPath = os.path.abspath(ips)
+        destDir, destFile = os.path.split(ipsPath)
+        patch = IPS_Patch.load(ips)
+        patchDict = patch.toDict()
+    else:
+        patchDict = common_patches.patches[ips]
+        destDir = sys.path[0] + "/../patches/common/ips"
+        destFile = ips + ".ips"
     destFile = "remove_" + destFile
     destPath = os.path.join(destDir, destFile)
-    patch = IPS_Patch.load(ips)
-    patchDict = patch.toDict()
     reversePatchDict = {}
     for addr, bytez in patchDict.items():
         sz = len(bytez)

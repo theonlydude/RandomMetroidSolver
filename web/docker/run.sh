544788ac9ebd2fd3429c6cc9b785ba9bfd0e69a0
kupppo <kupppo@protonmail.com>
2022-06-28 15:33:05 +0100

    run locally with volume for rapid changes

--- web/docker/run.sh
+++ web/docker/run.sh
@@ -2,11 +2,15 @@
 # run mysql & varia docker images
 # ./run.sh -b master
 # -b branch: optional branch checkouted during build (default to master)
+# -l local: if set, the docker image will use the local files instead of cloning
+#           the repository from github
 
 BRANCH="master"
-while getopts "b:" ARG; do
+LOCAL=""
+while getopts "b:l:" ARG; do
     case ${ARG} in
         b) export BRANCH="${OPTARG}";;
+        l) export LOCAL="${OPTARG}";;
 	*) echo "Unknown option ${ARG}"; exit 0;;
     esac
 done
@@ -14,7 +18,14 @@ done
 # mysql image run
 docker run --network varia-network --link varia-mysql:varia-mysql --name varia-mysql --publish 0.0.0.0:3366:3306 -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=varia -e MYSQL_USER=varia -e MYSQL_PASSWORD=varia -d varia-mysql
 
-docker run --network varia-network --link varia-${BRANCH}:varia-${BRANCH} -d --publish 0.0.0.0:8000:8000 --name varia-${BRANCH} varia-${BRANCH}
+if [ -n "${LOCAL}" ]; then
+    echo "local mode"
+    # TODO: Figure out how to get this path dynamically
+    docker run -v $(pwd)/web:/root/RandomMetroidSolver/web --network varia-network --link varia-${BRANCH}:varia-${BRANCH} -d --publish 0.0.0.0:8000:8000 --name varia-${BRANCH} varia-${BRANCH}
+else
+    echo "production mode"
+    docker run --network varia-network --link varia-${BRANCH}:varia-${BRANCH} -d --publish 0.0.0.0:8000:8000 --name varia-${BRANCH} varia-${BRANCH}
+fi
 
 # to check web2py logs:
 # docker exec varia-master ls -lhrt /var/log/supervisor/

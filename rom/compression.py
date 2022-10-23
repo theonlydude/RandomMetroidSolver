f622d4309c2296ac99fd265294adc3767d26718c
theonlydude <theonlydude@server.fake>
2022-10-23 17:42:17 +0200

    rom: add xor compression commands

--- rom/compression.py
+++ rom/compression.py
@@ -1,5 +1,19 @@
 # from https://github.com/DJuttmann/SM3E/blob/master/SM3E/Tools/Compression.cs
+import logging
 import utils.log
+from enum import IntEnum
+
+class Command(IntEnum):
+    DirectCopy = 0b000
+    ByteFill = 0b001
+    WordFill = 0b010
+    SigmaFill = 0b011
+    LibraryCopy = 0b100
+    EORedCopy = 0b101
+    MinusCopy = 0b110
+    EORedMinusCopy = 0b111
+    Long = 0b111
+    End = 0xff
 
 class Compressor:
     def __init__(self, computeLimit=5):
@@ -25,7 +39,7 @@ class Compressor:
             curAddress += 1
 
             # End of compressed data
-            if curByte == 0xFF:
+            if curByte == Command.End:
                 return (curAddress - startAddress, output)
 
             command = curByte >> 5
@@ -36,77 +50,85 @@ class Compressor:
             while True:
                 isLongLength = False
 
-                if command == 0b000:
+                if command == Command.DirectCopy:
                     # Copy source bytes
                     for i in range(length):
                         output.append(self._nextByte())
                     curAddress += length
-                    self.log.debug("Uncompressed: {}".format(output[-length:]))
+                    if self.log.getEffectiveLevel() == logging.DEBUG:
+                        self.log.debug("Uncompressed: {}".format(output[-length:]))
 
-                elif command == 0b001:
+                elif command == Command.ByteFill:
                     # Repeat one byte <length> times
                     copyByte = self._nextByte()
                     curAddress += 1
                     for i in range(length):
                         output.append(copyByte)
-                    self.log.debug("Repeat: {}".format(output[-length:]))
+                    if self.log.getEffectiveLevel() == logging.DEBUG:
+                        self.log.debug("Repeat: {}".format(output[-length:]))
 
-                elif command == 0b010:
+                elif command == Command.WordFill:
                     # Alternate between two bytes <length> times
                     copyByte1 = self._nextByte()
                     copyByte2 = self._nextByte()
                     curAddress += 2
                     for i in range(length):
                       output.append(copyByte1 if i % 2 == 0 else copyByte2)
-                    self.log.debug("Word: {}".format(output[-length:]))
+                    if self.log.getEffectiveLevel() == logging.DEBUG:
+                        self.log.debug("Word: {}".format(output[-length:]))
 
-                elif command == 0b011:
+                elif command == Command.SigmaFill:
                     # Sequence of increasing bytes
                     copyByte = self._nextByte()
                     curAddress += 1
                     for i in range(length):
                         output.append(copyByte)
                         copyByte += 1
-                    self.log.debug("Increment: {}".format(output[-length:]))
+                    if self.log.getEffectiveLevel() == logging.DEBUG:
+                        self.log.debug("Increment: {}".format(output[-length:]))
 
-                elif command == 0b100:
+                elif command == Command.LibraryCopy:
                     # Copy from output stream
                     outAddress = self._concatBytes(self._nextByte(), self._nextByte())
                     curAddress += 2
                     for i in range(length):
                         output.append(output[outAddress + i])
-                    self.log.debug("Copy: {}".format(output[-length:]))
+                    if self.log.getEffectiveLevel() == logging.DEBUG:
+                        self.log.debug("Copy: {}".format(output[-length:]))
 
-                elif command == 0b101:
+                elif command == Command.EORedCopy:
                     # Copy from output stream, flip bits
                     outAddress = self._concatBytes(self._nextByte(), self._nextByte())
                     curAddress += 2
                     for i in range(length):
                         output.append(output[outAddress + i] ^ 0xFF)
-                    self.log.debug("CopyXOR: {}".format(output[-length:]))
+                    if self.log.getEffectiveLevel() == logging.DEBUG:
+                        self.log.debug("CopyXOR: {}".format(output[-length:]))
 
-                elif command == 0b110:
+                elif command == Command.MinusCopy:
                     # Copy from output stream, relative to current index
                     outAddress = len(output) - self._nextByte()
                     curAddress += 1
                     for i in range(length):
                         output.append(output[outAddress + i])
-                    self.log.debug("RelativeCopy: {}".format(output[-length:]))
+                    if self.log.getEffectiveLevel() == logging.DEBUG:
+                        self.log.debug("RelativeCopy: {}".format(output[-length:]))
 
-                elif command == 0b111:
+                elif command == Command.Long:
                     # Long length (10 bits) command
                     command = (curByte >> 2) & 0b111;
                     length = ((curByte & 0b11) << 8) + self._nextByte() + 1;
                     curAddress += 1
                     self.log.debug("Long command")
 
-                    if command == 0b111:
+                    if command == Command.EORedMinusCopy:
                         # Copy output relative to current index, flip bits
                         outAddress = len(output) - self._nextByte()
                         curAddress += 1
                         for i in range(length):
                             output.append(output[outAddress + i] ^ 0xFF)
-                        self.log.debug("LongRelativeCopyXOR: {}".format(output[-length:]))
+                        if self.log.getEffectiveLevel() == logging.DEBUG:
+                            self.log.debug("LongRelativeCopyXOR: {}".format(output[-length:]))
                     else:
                         isLongLength = True;
 
@@ -126,15 +148,18 @@ class Compressor:
         self._computeWordFill(inputData)
         self._computeByteIncrement(inputData)
         self._computeCopy(inputData)
+        self._computeCopyXor(inputData)
 
         i = 0
         while i < len(inputData):
             length = max(self.byteFillLengths[i],
                          self.wordFillLengths[i],
                          self.byteIncrementLengths[i],
-                         self.copyLengths[i].length)
+                         self.copyLengths[i].length,
+                         self.copyLengthsXor[i].length)
 
-            self.log.debug("i:{} bf: {} wf: {} bi: {} c: {}".format(i, self.byteFillLengths[i], self.wordFillLengths[i], self.byteIncrementLengths[i], self.copyLengths[i].length))
+            if self.log.getEffectiveLevel() == logging.DEBUG:
+                self.log.debug("i:{} bf: {} wf: {} bi: {} c: {}".format(i, self.byteFillLengths[i], self.wordFillLengths[i], self.byteIncrementLengths[i], self.copyLengths[i].length))
 
             if length < 3:
                 j = i
@@ -166,10 +191,17 @@ class Compressor:
                 else:
                     self._writeCopy(self.copyLengths[i].address, length)
 
+            elif length == self.copyLengthsXor[i].length:
+                length = min(length, 1024)
+                if i - self.copyLengthsXor[i].address < 0xFF:
+                    self._writeNegativeCopyXor(i, i - self.copyLengthsXor[i].address, length)
+                else:
+                    self._writeCopyXor(self.copyLengthsXor[i].address, length)
+
             i += length
 
         # end of compressed data marker
-        self.output.append(0xFF)
+        self.output.append(Command.End)
 
         if len(self.output) > len(inputData):
             print("WARNING !!! len compressed {} > original data {}".format(len(self.output), len(inputData)))
@@ -180,48 +212,70 @@ class Compressor:
 
     def _writeChunkHeader(self, type, length):
         length -= 1
-        if length < 32:
+        if length < 32 and type != Command.EORedMinusCopy:
             # regular command
             self.output.append(type << 5 | length)
-            self.log.debug("_writeChunkHeader: cmd: {} len: {} value: {}".format(bin(type), length, type << 5 | length))
+            if self.log.getEffectiveLevel() == logging.DEBUG:
+                self.log.debug("_writeChunkHeader: cmd: {} len: {} value: {}".format(bin(type), length, type << 5 | length))
         else:
-            # long command
+            # long command or relative xor copy
             self.output.append(0b11100000 | type << 2 | length >> 8)
             self.output.append(length & 0xFF)
-            self.log.debug("_writeChunkHeader: long cmd: {} len: {} value: {} {}".format(bin(type), length, 0b11100000 | type << 2 | length >> 8, length & 0xFF))
+            if self.log.getEffectiveLevel() == logging.DEBUG:
+                self.log.debug("_writeChunkHeader: long cmd: {} len: {} value: {} {}".format(bin(type), length, 0b11100000 | type << 2 | length >> 8, length & 0xFF))
 
     def _writeUncompressed(self, inputData, index, length):
-        self._writeChunkHeader(0b000, length)
+        self._writeChunkHeader(Command.DirectCopy, length)
         self.output += inputData[index:index+length]
-        self.log.debug("_writeUncompressed: len: {} index: {} data: {}".format(length, index, inputData[index:index+length]))
+        if self.log.getEffectiveLevel() == logging.DEBUG:
+            self.log.debug("_writeUncompressed: len: {} index: {} data: {}".format(length, index, inputData[index:index+length]))
 
     def _writeByteFill(self, byte, length):
-        self._writeChunkHeader(0b001, length)
+        self._writeChunkHeader(Command.ByteFill, length)
         self.output.append(byte)
-        self.log.debug("_writeByteFill: len: {} byte: {}: {}".format(length, byte, [byte for i in range(length)]))
+        if self.log.getEffectiveLevel() == logging.DEBUG:
+            self.log.debug("_writeByteFill: len: {} byte: {}: {}".format(length, byte, [byte for i in range(length)]))
 
     def _writeWordFill(self, b0, b1, length):
-        self._writeChunkHeader(0b010, length)
+        self._writeChunkHeader(Command.WordFill, length)
         self.output.append(b0)
         self.output.append(b1)
-        self.log.debug("_writeWordFill: len: {} b0: {} b1: {}: {}".format(length, b0, b1, [b0 if i%2==0 else b1 for i in range(length)]))
+        if self.log.getEffectiveLevel() == logging.DEBUG:
+            self.log.debug("_writeWordFill: len: {} b0: {} b1: {}: {}".format(length, b0, b1, [b0 if i%2==0 else b1 for i in range(length)]))
 
     def _writeByteIncrement(self, byte, length):
-        self._writeChunkHeader(0b011, length)
+        self._writeChunkHeader(Command.SigmaFill, length)
         self.output.append(byte)
-        self.log.debug("_writeByteIncrement: len: {} byte: {}: {}".format(length, byte, [byte+i for i in range(length)]))
+        if self.log.getEffectiveLevel() == logging.DEBUG:
+            self.log.debug("_writeByteIncrement: len: {} byte: {}: {}".format(length, byte, [byte+i for i in range(length)]))
 
     def _writeCopy(self, address, length):
-        self._writeChunkHeader(0b100, length)
+        self._writeChunkHeader(Command.LibraryCopy, length)
         self.output.append(address & 0xFF)
         self.output.append(address >> 8)
-        self.log.debug("_writeCopy: {}".format(self.output[-3:]))
-        self.log.debug("_writeCopy: len: {} address: {}: {}".format(length, address, self.inputData[address:address+length]))
+        if self.log.getEffectiveLevel() == logging.DEBUG:
+            self.log.debug("_writeCopy: {}".format(self.output[-3:]))
+            self.log.debug("_writeCopy: len: {} address: {}: {}".format(length, address, self.inputData[address:address+length]))
+
+    def _writeCopyXor(self, address, length):
+        self._writeChunkHeader(Command.EORedCopy, length)
+        self.output.append(address & 0xFF)
+        self.output.append(address >> 8)
+        if self.log.getEffectiveLevel() == logging.DEBUG:
+            self.log.debug("_writeCopyXor: {}".format(self.output[-3:]))
+            self.log.debug("_writeCopyXor: len: {} address: {}: {}".format(length, address, self.inputData[address:address+length]))
 
     def _writeNegativeCopy(self, i, address, length):
-        self._writeChunkHeader(0b110, length)
+        self._writeChunkHeader(Command.MinusCopy, length)
         self.output.append(address)
-        self.log.debug("_writeNegativeCopy: len: {} address: {}: {}".format(length, address, self.inputData[i-address:i-address+length]))
+        if self.log.getEffectiveLevel() == logging.DEBUG:
+            self.log.debug("_writeNegativeCopy: len: {} address: {}: {}".format(length, address, self.inputData[i-address:i-address+length]))
+
+    def _writeNegativeCopyXor(self, i, address, length):
+        self._writeChunkHeader(Command.EORedMinusCopy, length)
+        self.output.append(address)
+        if self.log.getEffectiveLevel() == logging.DEBUG:
+            self.log.debug("_writeNegativeCopy: len: {} address: {}: {}".format(length, address, self.inputData[i-address:i-address+length]))
 
     def _computeByteFill(self, inputData):
         self.byteFillLengths = []
@@ -305,6 +359,42 @@ class Compressor:
         i = 0
         length = len(inputData)
         while b+i < length and inputData[a+i] == inputData[b+i]:
+            i += 1
+        #self.log.debug("_matchSubSequences a: {} b: {} i: {}".format(a,b,i))
+        return i
+
+    # same for xor values
+    def _computeCopyXor(self, inputData):
+        self.copyLengthsXor = []
+
+        # for each possible value store the positions of the value in the input data
+        start = [[] for i in range(len(inputData))]
+        for i in range(len(inputData)-1):
+            start[inputData[i] ^ 0xff].append(i)
+
+        for i, value in enumerate(inputData, start=0):
+            maxLength = 0
+            maxAddress = -1
+            for j, address in enumerate(start[inputData[i] ^ 0xff], start=0):
+                # for performance reasons limit the number of addresses
+                if j >= self.computeLimit:
+                    break
+                # only in previous addresses
+                if address >= i:
+                    break
+                length = self._matchSubSequencesXor(address, i, inputData)
+                if length > maxLength:
+                    maxLength = length
+                    maxAddress = address
+            self.copyLengthsXor.append(Compressor._Interval(maxAddress, maxLength))
+
+    def _matchSubSequencesXor(self, a, b, inputData):
+        if a >= b:
+            return 0
+
+        i = 0
+        length = len(inputData)
+        while b+i < length and (inputData[a+i] ^ 0xff) == inputData[b+i]:
             i += 1
         #self.log.debug("_matchSubSequences a: {} b: {} i: {}".format(a,b,i))
         return i

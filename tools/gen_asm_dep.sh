dabd1eb371fc366d401316118bc45e7670e8d122
flo <flo@flo.flo>
2022-11-09 00:22:21 +0100

    tools: made asm dep tool recursive

--- tools/gen_asm_dep.sh
+++ tools/gen_asm_dep.sh
@@ -3,17 +3,23 @@
 src=$1
 
 incdirs="$(dirname $src) ${INCLUDE_DIRS}"
-includes=$(grep '^incsrc' $src | cut -f 2 -d ' ' | sed -e "s/'//g" -e 's/"//g')
 
 printf "${src}:\t"
 
-for inc in ${includes}; do
-    for incdir in ${incdirs}; do
-        incfile=${incdir}/${inc}
-        [ -f "${incfile}" ] && {
-            printf " ${incfile}"
-            break
-        }
+function add_deps() {
+    includes=$(grep '^incsrc' $1 | cut -f 2 -d ' ' | sed -e "s/'//g" -e 's/"//g')
+    for inc in ${includes}; do
+        for incdir in ${incdirs}; do
+            incfile=${incdir}/${inc}
+            [ -f "${incfile}" ] && {
+                printf " ${incfile}"
+                add_deps ${incfile}
+                break
+            }
+        done
     done
-done
+}
+
+add_deps $src
+
 printf "\n\t@touch ${src}\n"

95f598ac345f2a97ef3365b0cb3811aacab3aa90
theonlydude <theonlydude@server.fake>
2022-11-10 11:41:30 +0100

    rom: add vanilla doors labels in bank 8f

---
+++ patches/vanilla/src/bank_8f.asm
@@ -0,0 +1,286 @@
+org $8f8018
+Door_00_Room_91F8_PLM_C872:
+org $8fc881
+Door_00_Room_DE4D_PLM_C8CA:
+org $8f801e
+Door_01_Room_91F8_PLM_C85A:
+org $8f8044
+Door_02_Room_91F8_PLM_C848:
+org $8f804a
+Door_03_Room_91F8_PLM_C842:
+org $8f8050
+Door_04_Room_91F8_PLM_C842:
+org $8f80fc
+Door_05_Room_92FD_PLM_C88A:
+org $8f81ac
+Door_06_Room_92FD_PLM_C848:
+org $8f81b2
+Door_07_Room_92FD_PLM_C848:
+org $8f81b8
+Door_08_Room_92FD_PLM_C848:
+org $8f81be
+Door_09_Room_92FD_PLM_C842:
+org $8f81c4
+Door_0A_Room_92FD_PLM_C84E:
+org $8f81dc
+Door_0B_Room_93FE_PLM_C842:
+org $8f81e2
+Door_0C_Room_93FE_PLM_C872:
+org $8f8222
+Door_0D_Room_948C_PLM_C85A:
+org $8f8228
+Door_0E_Room_948C_PLM_C866:
+org $8f823e
+Door_0F_Room_95A8_PLM_C866:
+org $8f8256
+Door_10_Room_962A_PLM_C86C:
+org $8f82f8
+Door_11_Room_96BA_PLM_C842:
+org $8f82fe
+Door_12_Room_96BA_PLM_C848:
+org $8f8304
+Door_13_Room_96BA_PLM_C85A:
+org $8f839c
+Door_14_Room_96BA_PLM_C842:
+org $8f83a2
+Door_15_Room_96BA_PLM_C842:
+org $8f83a8
+Door_16_Room_96BA_PLM_C842:
+org $8f83ae
+Door_17_Room_96BA_PLM_C848:
+org $8f83c8
+Door_18_Room_975C_PLM_C848:
+org $8f83e2
+Door_19_Room_975C_PLM_C842:
+org $8f83e8
+Door_1A_Room_975C_PLM_C848:
+org $8f83fe
+Door_1B_Room_9804_PLM_BAF4:
+org $8f8418
+Door_1C_Room_9804_PLM_C848:
+org $8f8420
+Door_1D_Room_9879_PLM_C88A:
+org $8f8470
+Door_1E_Room_99BD_PLM_C88A:
+org $8f84b2
+Door_1F_Room_9AD9_PLM_C890:
+org $8f84b8
+Door_20_Room_9AD9_PLM_C890:
+org $8f84be
+Door_21_Room_9AD9_PLM_C88A:
+org $8f84c4
+Door_22_Room_9AD9_PLM_C88A:
+org $8f84ca
+Door_23_Room_9AD9_PLM_C890:
+org $8f84d0
+Door_24_Room_9AD9_PLM_C842:
+org $8f84ec
+Door_25_Room_9B9D_PLM_C842:
+org $8f8512
+Door_26_Room_9BC8_PLM_C88A:
+org $8f8548
+Door_27_Room_9C5E_PLM_C878:
+org $8f861a
+Door_28_Room_9D19_PLM_C85A:
+org $8f8620
+Door_29_Room_9D19_PLM_C872:
+org $8f8626
+Door_2A_Room_9D19_PLM_C88A:
+org $8f862c
+Door_2B_Room_9D19_PLM_C890:
+org $8f8634
+Door_2C_Room_9D9C_PLM_C848:
+org $8f863a
+Door_2D_Room_9D9C_PLM_C854:
+org $8f8642
+Door_2E_Room_9DC7_PLM_C87E:
+org $8f8656
+Door_2F_Room_9E11_PLM_C842:
+org $8f8670
+Door_30_Room_9E52_PLM_C85A:
+org $8f8746
+Door_31_Room_9E9F_PLM_C848:
+org $8f8784
+Door_32_Room_9F11_PLM_C88A:
+org $8f87a6
+Door_33_Room_9FBA_PLM_C872:
+org $8f87c8
+Door_34_Room_A011_PLM_C878:
+org $8f87e0
+Door_35_Room_A0A4_PLM_C878:
+org $8f8816
+Door_36_Room_A130_PLM_C848:
+org $8f881c
+Door_37_Room_A130_PLM_C842:
+org $8f8860
+Door_38_Room_A253_PLM_C878:
+org $8f8866
+Door_39_Room_A253_PLM_C860:
+org $8f886e
+Door_3A_Room_A293_PLM_C890:
+org $8f88aa
+Door_3B_Room_A322_PLM_C878:
+org $8f88b0
+Door_3C_Room_A322_PLM_C860:
+org $8f88b6
+Door_3D_Room_A322_PLM_C878:
+org $8f88d0
+Door_3E_Room_A37C_PLM_C842:
+org $8f8966
+Door_3F_Room_A408_PLM_C872:
+org $8f898e
+Door_40_Room_A471_PLM_C848:
+org $8f8996
+Door_41_Room_A4B1_PLM_C842:
+org $8f89f4
+Door_42_Room_A521_PLM_C842:
+org $8f89fa
+Door_43_Room_A521_PLM_C848:
+org $8f8a14
+Door_44_Room_A56B_PLM_C872:
+org $8f8a1a
+Door_45_Room_A56B_PLM_DB5A:
+org $8f8a2e
+Door_46_Room_A59F_PLM_C842:
+org $8f8a34
+Door_47_Room_A59F_PLM_C848:
+org $8f8a54
+Door_48_Room_A66A_PLM_C848:
+org $8f8aea
+Door_49_Room_A788_PLM_C872:
+org $8f8af2
+Door_4A_Room_A7B3_PLM_C88A:
+org $8f8b00
+Door_4B_Room_A7DE_PLM_C878:
+org $8f8b06
+Door_4C_Room_A7DE_PLM_C860:
+org $8f8b0c
+Door_4D_Room_A7DE_PLM_C890:
+org $8f8b96
+Door_4E_Room_A923_PLM_C87E:
+org $8f8b9e
+Door_4F_Room_A98D_PLM_C854:
+org $8f8be0
+Door_50_Room_AA41_PLM_C842:
+org $8f8bf4
+Door_51_Room_AA82_PLM_C890:
+org $8f8c0c
+Door_52_Room_AB07_PLM_C88A:
+org $8f8c5a
+Door_53_Room_ACB3_PLM_C878:
+org $8f8c60
+Door_54_Room_ACB3_PLM_C872:
+org $8f8c7a
+Door_55_Room_ACF0_PLM_C88A:
+org $8f8ca8
+Door_56_Room_AD5E_PLM_C88A:
+org $8f8cc2
+Door_57_Room_ADAD_PLM_C88A:
+org $8f8d4e
+Door_58_Room_AE74_PLM_C860:
+org $8f8e7a
+Door_59_Room_B283_PLM_C842:
+org $8f8e98
+Door_5A_Room_B32E_PLM_C842:
+org $8f8e9e
+Door_5B_Room_B32E_PLM_C848:
+org $8f8ea6
+Door_5C_Room_B37A_PLM_DB4C:
+org $8f8f2a
+Door_5D_Room_B40A_PLM_C848:
+org $8f902e
+Door_5E_Room_B585_PLM_C866:
+org $8f90ba
+Door_5F_Room_B5D5_PLM_C878:
+org $8f90c8
+Door_60_Room_B62B_PLM_C848:
+org $8fc231
+Door_80_Room_CA52_PLM_C848:
+org $8fc237
+Door_81_Room_CA52_PLM_C842:
+org $8fc26b
+Door_82_Room_CAF6_PLM_C848:
+org $8fc271
+Door_83_Room_CAF6_PLM_C842:
+org $8fc277
+Door_84_Room_CAF6_PLM_C87E:
+org $8fc29d
+Door_85_Room_CC6F_PLM_DB5A:
+org $8fc2b3
+Door_86_Room_CD13_PLM_C848:
+org $8fc2f5
+Door_87_Room_C98E_PLM_C848:
+org $8fc2ff
+Door_88_Room_CA52_PLM_C848:
+org $8fc305
+Door_89_Room_CA52_PLM_C842:
+org $8fc30b
+Door_8A_Room_CA52_PLM_C84E:
+org $8fc32f
+Door_8B_Room_CBD5_PLM_C890:
+org $8fc3d7
+Door_8C_Room_CEFB_PLM_C88A:
+org $8fc431
+Door_8D_Room_CFC9_PLM_C88A:
+org $8fc475
+Door_8E_Room_D017_PLM_C88A:
+org $8fc4fb
+Door_8F_Room_D1A3_PLM_C872:
+org $8fc547
+Door_90_Room_D21C_PLM_C88A:
+org $8fc553
+Door_91_Room_D2AA_PLM_C848:
+org $8fc569
+Door_92_Room_D30B_PLM_C88A:
+org $8fc571
+Door_93_Room_D340_PLM_C842:
+org $8fc577
+Door_94_Room_D340_PLM_C87E:
+org $8fc5d3
+Door_95_Room_D48E_PLM_C884:
+org $8fc5fd
+Door_96_Room_D5A7_PLM_C890:
+org $8fc611
+Door_97_Room_D5EC_PLM_C842:
+org $8fc6ef
+Door_98_Room_D72A_PLM_C88A:
+org $8fc6f5
+Door_99_Room_D72A_PLM_C872:
+org $8fc6fb
+Door_9A_Room_D72A_PLM_C872:
+org $8fc73b
+Door_9B_Room_D78F_PLM_DB4C:
+org $8fc797
+Door_9C_Room_D913_PLM_C848:
+org $8fc79f
+Door_9D_Room_D95E_PLM_C848:
+org $8fc7bb
+Door_9E_Room_DA60_PLM_C842:
+org $8fc7c1
+Door_9F_Room_DA60_PLM_C848:
+org $8fc7ef
+Door_A0_Room_DAE1_PLM_C848:
+org $8fc7fd
+Door_A1_Room_DB31_PLM_C842:
+org $8fc80b
+Door_A2_Room_DB7D_PLM_C842:
+org $8fc819
+Door_A3_Room_DBCD_PLM_C84E:
+org $8fc823
+Door_A4_Room_DC65_PLM_C842:
+org $8fc829
+Door_A5_Room_DC65_PLM_C848:
+org $8fc831
+Door_A6_Room_DCB1_PLM_C842:
+org $8fc839
+Door_A7_Room_DCFF_PLM_C88A:
+org $8fc857
+Door_A8_Room_DDC4_PLM_DB5A:
+org $8fc86b
+Door_A9_Room_DDF3_PLM_C890:
+org $8fc88f
+Door_AA_Room_DE7A_PLM_C854:
+org $8fc89d
+Door_AB_Room_DEA7_PLM_C848:
+org $8fc8ab
+Door_AC_Room_DEDE_PLM_C848:

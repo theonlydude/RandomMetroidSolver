471ca8f7b064e4c8e6753892e3570ddf9ea0cc8b
flo <flo@flo.flo>
2022-11-12 17:39:30 +0100

    rom: removed duplicate symbols for MESEN debug

---
+++ patches/common/src/include/constants.asm
@@ -0,0 +1,34 @@
+;;; VARIA shared constants
+
+include
+
+;; RTA timer RAM updated during NMI
+!timer1 = $05b8
+!timer2 = $05ba
+
+;; stats RAM
+!_stats_ram = fc00
+!stats_ram = $7f!_stats_ram
+!stats_timer = !stats_ram
+
+;; tracked stats (see tracking.txt)
+!stat_nb_door_transitions = #$0002
+!stat_rta_door_transitions = #$0003
+!stat_rta_door_align = #$0005
+!stat_rta_regions = #$0007
+!stat_uncharged_shots = #$001f
+!stat_charged_shots = #$0020
+!stat_SBAs = #$0021
+!stat_missiles = #$0022
+!stat_supers = #$0023
+!stat_PBs = #$0024
+!stat_bombs = #$0025
+!stat_rta_menu = #$0026
+!stat_deaths = #$0028
+!stat_resets = #$0029
+
+;;; IGT vanilla RAM
+!igt_frames = $7E09DA
+!igt_seconds = $7E09DC
+!igt_minutes = $7E09DE
+!igt_hours = $7E09E0

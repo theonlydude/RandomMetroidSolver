bbcd63c6657315f88827b00f545efd6b39809be8
theonlydude <theonlydude@server.fake>
2022-12-19 12:11:52 +0100

    rom: update bowling alley item addresses

--- graph/mirror/graph_locations.py
+++ graph/mirror/graph_locations.py
@@ -34,5 +34,10 @@ locationsDict["Missile (Grapple Beam)"].Available = (
 # croc room plm's have been relocated in 8f freespace
 # TODO::use symbol Energy_Tank_Crocomire in bank_8f to get its address
 locationsDict["Energy Tank, Crocomire"].Address = snes_to_pc(0x8ffe5f)
+# TODO::use symbol Room_C98E_Reserve in bank_8f to get its address
+locationsDict["Reserve Tank, Wrecked Ship"].Address = snes_to_pc(0x8ffe00)
+# TODO::use symbol Room_C98E_Missile in bank_8f to get its address
+locationsDict["Missile (Gravity Suit)"].Address = snes_to_pc(0x8ffe06)
+
 
 locations = [loc for loc in locationsDict.values()]

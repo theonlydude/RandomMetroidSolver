0605d5c0920716568af24c281eceb453d8894b6a
flo <flo@flo.flo>
2023-01-07 16:49:33 +0100

    web: made symbols server a usable function

--- symbolsd.py
+++ symbolsd.py
@@ -6,20 +6,22 @@ from logic.logic import Logic
 from rom.flavor import RomFlavor, SymbolsManager
 from rom.symbols import Symbols
 from patches.patchaccess import PatchAccess
-from multiprocessing import Process
-from multiprocessing.managers import BaseManager
 
-if len(sys.argv) < 2:
-    print("args: logic")
-    sys.exit(1)
+def symbolsServer(flavor):
+    Logic.factory(flavor)
 
-Logic.factory(sys.argv[1])
+    symbols = Symbols(PatchAccess())
+    symbols.loadAllSymbols()
+    SymbolsManager.register("Symbols", lambda: symbols)
 
-symbols = Symbols(PatchAccess())
-symbols.loadAllSymbols()
-SymbolsManager.register("Symbols", lambda: symbols)
+    port = RomFlavor.ports[Logic.implementation]
+    manager = SymbolsManager(address=("localhost", port), authkey=b'password')
+    server = manager.get_server()
+    server.serve_forever()
 
-port = RomFlavor.ports[Logic.implementation]
-manager = SymbolsManager(address=("localhost", port), authkey=b'password')
-server = manager.get_server()
-server.serve_forever()
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("args: logic")
+        sys.exit(1)
+    symbolsServer(sys.argv[1])
+

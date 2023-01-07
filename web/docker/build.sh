41390adde61f1ad09ebedd9ff329f471f6e73f92
flo <flo@flo.flo>
2023-01-07 17:34:30 +0100

    web: use host network for docker build to fix WSL issue

--- web/docker/build.sh
+++ web/docker/build.sh
@@ -57,10 +57,10 @@ if [ -n "${GITHUB_TOKEN}" ]; then
 fi
 
 if [ ${LOCAL} -eq 1 ]; then
-    docker build --tag varia-${BRANCH} --build-arg BRANCH=${BRANCH} --build-arg GITHUB_TOKEN=${GITHUB_TOKEN} -f web2py/Dockerfile web2py/
+    docker build --network=host --tag varia-${BRANCH} --build-arg BRANCH=${BRANCH} --build-arg GITHUB_TOKEN=${GITHUB_TOKEN} -f web2py/Dockerfile web2py/
     rm -f web2py/RandomMetroidSolver.tar.gz
 else
-    docker build --tag varia-${BRANCH} -f web2py/Dockerfile.local $(get_dir)
+    docker build --network=host --tag varia-${BRANCH} -f web2py/Dockerfile.local $(get_dir)
 fi
 
 rm -f mysql/*.sql

02d0916ad9d374af1f7b000e2f8330c881b91750
theonlydude <theonlydude@server.fake>
2023-01-12 15:18:10 +0100

    preset: mention spring ball jumps for level 1 underwater suitless maridia

--- utils/parameters.py
+++ utils/parameters.py
@@ -261,7 +261,7 @@ class Knows:
     # underwater grav-less
     GravLessLevel1 = SMBool(True, hardcore, ['GravLessLevel1'])
     desc['GravLessLevel1'] = {'display': 'Level 1',
-                              'title': 'Make your way underwater with Hi-Jump and Ice, freezing crabs and fishes. Access Botwoon with grapple.',
+                              'title': 'Make your way underwater with Hi-Jump, and Ice for freezing crabs and fishes or Spring Ball for spring ball jumps. Access Botwoon with grapple.',
                               'href': 'https://www.youtube.com/watch?v=G5bB03j5GZc',
                               'rooms': ['Main Street', 'Mt. Everest', 'Crab Shaft', 'Pseudo Plasma Spark Room',
                                         'Aqueduct', 'Botwoon Hallway', "Botwoon's Room", 'Botwoon Energy Tank Room',

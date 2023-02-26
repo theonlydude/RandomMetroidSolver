3ca00e75d60bbdaf1b8a6db176aeac51551b41ba
theonlydude <theonlydude@server.fake>
2023-02-26 11:28:36 +0100

    rando: other way to fix previous issue

--- graph/mirror/graph_locations.py
+++ graph/mirror/graph_locations.py
@@ -32,9 +32,10 @@ locationsDict["Missile (Grapple Beam)"].Available = (
 )
 
 def fixLocAddresses():
-    # some PLMs have been relocated in 8f freespace
-    locationsDict["Energy Tank, Crocomire"].Address = Addresses.getOne("bank_8f_Energy_Tank_Crocomire")
-    locationsDict["Reserve Tank, Wrecked Ship"].Address = Addresses.getOne("bank_8f_Room_C98E_Reserve")
-    locationsDict["Missile (Gravity Suit)"].Address = Addresses.getOne("bank_8f_Room_C98E_Missile")
+    # some PLMs have been relocated in 8f freespace.
+    # change Address in class and not instance so that copy use the updated address
+    type(locationsDict["Energy Tank, Crocomire"]).Address = Addresses.getOne("bank_8f_Energy_Tank_Crocomire")
+    type(locationsDict["Reserve Tank, Wrecked Ship"]).Address = Addresses.getOne("bank_8f_Room_C98E_Reserve")
+    type(locationsDict["Missile (Gravity Suit)"]).Address = Addresses.getOne("bank_8f_Room_C98E_Missile")
 
 locations = [loc for loc in locationsDict.values()]

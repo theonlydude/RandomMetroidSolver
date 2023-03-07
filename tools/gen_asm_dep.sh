8c3f03ec2d33322126b046efc9b33b08e8246879
flo <flo@flo.flo>
2023-03-07 01:44:21 +0100

    tools: fixed huge bug in asm dep tool

--- tools/gen_asm_dep.sh
+++ tools/gen_asm_dep.sh
@@ -6,27 +6,38 @@ ips=$2
 printf "${ips}:\t${src}"
 
 function add_deps() {
-    incdirs="$(dirname $1) ${INCLUDE_DIRS}"
-    includes=$(grep -i '^incsrc' $1 | cut -f 2 -d ' ' | sed -e "s/'//g" -e 's/"//g')
-    incbins=$(grep -i '^incbin' $1 | cut -f 2 -d ' ' | sed -e "s/'//g" -e 's/"//g')
+    local incdirs="$(dirname $1) ${INCLUDE_DIRS}"
+    local includes=$(grep -i '^incsrc' $1 | cut -f 2 -d ' ' | sed -e "s/'//g" -e 's/"//g')
+    local incbins=$(grep -i '^incbin' $1 | cut -f 2 -d ' ' | sed -e "s/'//g" -e 's/"//g')
+    echo "BINARIES" >&2
+    echo "incbins = ${incbins}" >&2
     for inc in ${incbins}; do
+        echo "bin inc ${inc}" >&2
         for incdir in ${incdirs}; do
-            incfile=${incdir}/${inc}
+            local incfile=${incdir}/${inc}
+            echo "incfile ${incfile}" >&2
             [ -f "${incfile}" ] && {
+                echo "OK!" >&2
                 printf " ${incfile}"
                 break
             }
         done
     done
+    echo "SRCS" >&2
+    echo "includes = ${includes}" >&2
     for inc in ${includes}; do
+        echo "src inc ${inc}" >&2
         for incdir in ${incdirs}; do
-            incfile=${incdir}/${inc}
+            local incfile=${incdir}/${inc}
+            echo "incfile ${incfile}" >&2
             [ -f "${incfile}" ] && {
+                echo "OK!" >&2
                 printf " ${incfile}"
                 add_deps ${incfile}
                 break
             }
             [[ "$SYM_ASM_FILES" == *"$incfile"* ]] && {
+                echo "SYM OK!" >&2
                 # we reference a symbol export file that does not exist yet
                 printf " ${incfile}"
             }

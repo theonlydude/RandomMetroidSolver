a96779c7800c64c9b01ef35530cd693b4f451cb7
flo <flo@flo.flo>
2023-03-12 23:10:09 +0100

    map: added missing map data

--- patches/vanilla/src/map_data.asm
+++ patches/vanilla/src/map_data.asm
@@ -9,3 +9,13 @@ incbin "map/wrecked_ship.bin"
 incbin "map/maridia.bin"
 incbin "map/tourian.bin"
 incbin "map/ceres.bin"
+
+org $829727
+
+incbin "map/crateria_data.bin"
+incbin "map/brinstar_data.bin"
+incbin "map/norfair_data.bin"
+incbin "map/wrecked_ship_data.bin"
+incbin "map/maridia_data.bin"
+incbin "map/tourian_data.bin"
+incbin "map/ceres_data.bin"

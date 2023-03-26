7f8e4e015691773a1477e887f2f09881dbc62560
flo <flo@flo.flo>
2023-03-26 17:21:13 +0200

    tools: bin2asm improvements

--- tools/bin_to_asm_data.py
+++ tools/bin_to_asm_data.py
@@ -8,7 +8,7 @@ sys.path.append(os.path.dirname(sys.path[0]))
 from rom.rom import pc_to_snes, RealROM
 from rom.ips import IPS_Patch
 
-def writeAsm(binary, outAsm, start, end):
+def writeAsm(binary, outAsm, start, end, chunk):
     print("Writing " + outAsm + "...")
     b=RealROM(binary)
     with open(outAsm, 'w') as f:
@@ -18,9 +18,11 @@ def writeAsm(binary, outAsm, start, end):
         while not eof():
             f.write("org $%06x\n" % addr)
             f.write("\tdw ")
-            for i in range(32):
+            for i in range(chunk):
                 w = b.readWord()
-                f.write("$%04x," % w)
+                if i > 0:
+                    f.write(", ")
+                f.write("$%04x" % w)
                 addr += 2
                 if eof():
                     break
@@ -29,5 +31,6 @@ def writeAsm(binary, outAsm, start, end):
 b = sys.argv[1]
 start = int(sys.argv[2], 16)
 end = int(sys.argv[3], 16)
+chunk = int(sys.argv[4]) if len(sys.argv) > 4 else 32
 outAsm = os.path.splitext(b)[0] + ".asm"
-writeAsm(b, outAsm, start, end)
+writeAsm(b, outAsm, start, end, chunk)

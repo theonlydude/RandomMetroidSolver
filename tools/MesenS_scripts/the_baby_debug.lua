da3546220c1dd1cd5116a4a6e4d3d417e6ae3fe3
theonlydude <theonlydude@server.fake>
2023-04-21 17:26:42 +0200

    tools: add mesen debug scripts for mother brain

--- tools/MesenS_scripts/the_baby_debug.lua
+++ tools/MesenS_scripts/the_baby_debug.lua
@@ -35,77 +35,50 @@ end
 --        $0FB4: Parameter 1 (Speed in SMILE)
 --        $0FB6: Parameter 2 (Speed2 in SMILE)
 
-local etecoonData = {
+local babyData = {
       {"F7A Xpos", addr=0x0F7A},
       {"F7E Ypos", addr=0x0F7E},
-      {"FB2 AI", addr=0x0FB2},
+      {"FB2 timer", addr=0x0FB2},
       {"F92 InsList", addr=0x0F92},
       {"F8E Sprmap", addr=0x0F8E},
-      {"FA8 var0", addr=0x0FA8},
+      {"FA8 AI", addr=0x0FA8},
       {"FAA var1", addr=0x0FAA},
       {"FAC var2", addr=0x0FAC},
       {"FAE var3", addr=0x0FAE},
       {"FB0 var4", addr=0x0FB0},
+      {"7814 angle", addr=0x7e7814},
+      {"7816 speed", addr=0x7e7816},
 }
-
--- keep history of ai changes
-etecoons_history = {}
-etecoons_history[0x200] = 0x0
-etecoons_history[0x240] = 0x0
-etecoons_history[0x280] = 0x0
-
-etecoons_colors = {}
-etecoons_colors[0x200] = 0x30FF4040
-etecoons_colors[0x240] = 0x3040FF40
-etecoons_colors[0x280] = 0x304040FF
 
 local X_OFF = 125
 local Y_OFF = 10
 local BG = 0x80000000
 local FG = 0xFFFFFF
 
-local function printEtecoons()
+local function printBaby()
   local x,y=0,0
   local function printVar(var, value, color)
      emu.drawString(x+2, y+2, var .. ":", color, BG, 1)
      emu.drawString(x+2+X_OFF//2, y+2, string.format("%x", value), color, BG, 1)
   end
 
-  for _, offset in ipairs({0x200, 0x240, 0x280}) do
-     local color = etecoons_colors[offset]
-     for i,info in pairs(etecoonData) do
-        local var = info[1]
-        local value = readWord(info.addr+offset)
-        printVar(var, value, color)
-        if i % 2 == 1 then
-           x = X_OFF
-        else
-           x = 0
-           y = y + Y_OFF
-        end
+  local offset = 0xc0
+  local color = 0xFFFFFF
+  for i,info in pairs(babyData) do
+     local var = info[1]
+     local value = readWord(info.addr+offset)
+     printVar(var, value, color)
+     if i % 2 == 1 then
+        x = X_OFF
+     else
+        x = 0
+        y = y + Y_OFF
      end
-     y = y+(Y_OFF*2)
-
-     -- get ai
-     local ai = readWord(0x0FB2+offset)
-     local il = readWord(0x0F92+offset)
-
-     -- compare with last one, log if different
-     if(ai ~= etecoons_history[offset]) then
-        emu.log(string.format("%x: ai: %x il: %x", offset, ai, il))
-        etecoons_history[offset] = ai
-     end
-
-     -- display colored box for each etecoon
-     local x = readWord(0x0F7A+offset)+16
-     local y = readWord(0x0F7E+offset)
-
-     -- camera
-     local layer1x = readWord(0x0911)
-     local layer1y = readWord(0x0915)
-
-     emu.drawRectangle(x-layer1x, y-layer1y, 8, 8, color, true)
   end
+  y = y+(Y_OFF*2)
+
+  emu.log(string.format("x: %x y: %x angle: %x speed: %x", readWord(0x0f7a+offset), readWord(0x0f7e+offset), readWord(0x7e7814+offset), readWord(0x7e7816+offset)))
+
 end
 
-emu.addEventCallback(printEtecoons, emu.eventType.endFrame)
+emu.addEventCallback(printBaby, emu.eventType.endFrame)

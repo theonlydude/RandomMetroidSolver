872b5c0ff9bd1991b1cfd8b4908fc8d538c8a9f8
theonlydude <theonlydude@server.fake>
2023-06-25 19:39:46 +0200

    web: add shell script to get latest tracker

---
+++ web/pull_latest_tracker.sh
@@ -0,0 +1,10 @@
+echo "begin tracker pull"
+cd $(dirname $0)
+
+NEW_RELEASE=$(curl -s https://api.github.com/repos/chriscauley/super-metroid/releases/latest | jq -r '.assets[0].browser_download_url')
+
+rm -rf client
+echo "${NEW_RELEASE}" | wget -qi - && tar zxf release.tar.gz && rm -f release.tar.gz
+./install.sh
+
+echo "end tracker pull"
